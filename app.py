c2f08f5db055f558306eb8688b9a9e51ddbd61f5
dr-rodriguez <strakul@gmail.com>
2016-02-18 16:55:47 -0500

    using to_pandas() now from astropy; only SELECT commands allowed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,redirect
 from astrodbkit import astrodb
-import pandas as pd
+#import pandas as pd
 
 
 app_bdnyc = Flask(__name__)
@@ -10,6 +10,7 @@ app_bdnyc = Flask(__name__)
 # To access the query form
 
 app_bdnyc.vars={}
+app_bdnyc.vars['query'] = ''
 
 @app_bdnyc.route('/')
 def bdnyc_home():
@@ -23,37 +24,38 @@ def bdnyc_index():
 @app_bdnyc.route('/query', methods=['GET','POST'])
 def bdnyc_query():
     defquery = 'SELECT * FROM sources LIMIT 25'
+    if app_bdnyc.vars['query']=='':
+        app_bdnyc.vars['query'] = defquery
 
     if request.method == 'GET':
-        return render_template('query.html', defquery=defquery)
+        return render_template('query.html', defquery=app_bdnyc.vars['query'])
     else:
         return 'This was a POST request'
 
 @app_bdnyc.route('/runquery', methods=['POST'])
 def bdnyc_runquery():
     app_bdnyc.vars['query'] = request.form['query_to_run']
+    htmltxt = app_bdnyc.vars['query'].replace('<','&lt;')
 
     # Load the database
     db = astrodb.get_db('./BDNYCv1.0.db')
+
+    # Only SELECT commands are allowed
+    if not app_bdnyc.vars['query'].lower().startswith('select'):
+        return render_template('view.html', table='<p>Only SELECT queries are allowed. You typed:<br>'+htmltxt+'</p>')
 
     # Run the query
     try:
         t = db.query(app_bdnyc.vars['query'], fmt='table')
     except ValueError:
         t = db.query(app_bdnyc.vars['query'], fmt='array')
-        htmltxt = app_bdnyc.vars['query'].replace('<','&lt;')
         if len(t)==0: # no entries found
             return render_template('view.html', table='<p>No entries found for query:<br>'+htmltxt+'</p>')
-        else:
-            return render_template('view.html', table='<p>Error for query:<br>'+htmltxt+'</p>')
 
     # Convert to Pandas data frame
     try:
-        data = pd.DataFrame()
-        for col in t.keys():
-            data.loc[:,col] = pd.Series(t[col])
+        data = t.to_pandas()
     except AttributeError:
-        htmltxt = app_bdnyc.vars['query'].replace('<','&lt;')
         return render_template('view.html', table='<p>Error for query:<br>'+htmltxt+'</p>')
 
     return render_template('view.html', table=data.to_html(classes='display', index=False))
