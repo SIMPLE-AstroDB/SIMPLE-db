514efc485cb48ba6527d22463b1e0f419a7a8394
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-03-08 10:54:08 -0500

    set debug to False

--- app.py
+++ app.py
@@ -220,4 +220,4 @@ def bdnyc_inventory():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app_bdnyc.run(host='0.0.0.0', port=port, debug=True)
+    app_bdnyc.run(host='0.0.0.0', port=port, debug=False)
