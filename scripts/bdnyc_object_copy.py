ef8795ff8e440c5daa12b87e5f5c441c639a313d
David Rodriguez <drodriguez@stsci.edu>
2020-06-30 17:09:54 -0400

    first pass at loading some BDNYC sources into SIMPLE

--- scripts/bdnyc_object_copy.py
+++ scripts/bdnyc_object_copy.py
@@ -1,7 +1,8 @@
 # Script to copy over BDNYC database sources into SIMPLE
 
-from astrodbkit2.astrodb import Database, copy_database_schema
+from astrodbkit2.astrodb import Database, and_
 from sqlalchemy import types  # for BDNYC column overrides
+from collections import Counter
 
 # Establish connection to databases
 
@@ -39,3 +40,54 @@ db.Publications.insert().execute(data)
 # Verify insert and save to disk
 db.query(db.Publications).count()
 db.save_db('data')
+
+# ----------------------------------------------------------------------------------------
+# Add Sources that are not already in SIMPLE
+temp = db.query(db.Publications.c.name).all()
+publications_simple = [s[0] for s in temp]
+temp = db.query(db.Sources.c.source).all()
+existing_simple = [s[0] for s in temp]
+
+temp = bdnyc.query(bdnyc.sources.c.designation,
+                   bdnyc.sources.c.names,
+                   bdnyc.sources.c.ra,
+                   bdnyc.sources.c.dec,
+                   bdnyc.sources.c.shortname,
+                   bdnyc.sources.c.publication_shortname, )\
+    .filter(and_(bdnyc.sources.c.names.notin_(existing_simple),
+                 bdnyc.sources.c.designation.notin_(existing_simple),
+                 bdnyc.sources.c.publication_shortname.in_(publications_simple),
+                 bdnyc.sources.c.version <= 2,
+                 bdnyc.sources.c.designation.isnot(None),
+                 bdnyc.sources.c.ra.isnot(None),
+                 bdnyc.sources.c.dec.isnot(None),
+                 bdnyc.sources.c.publication_shortname.isnot(None)))\
+    .all()
+
+# Restructure for input into SIMPLE
+new_db_mapping = {'designation': 'source', 'name': 'shortname', 'publication_shortname': 'reference'}
+data = [{new_db_mapping.get(k, k): x.__getattribute__(k)
+         for k in x.keys() if k not in ('id', 'names')
+         }
+        for x in temp]
+
+# Eliminate duplicated rows (SIMPLE requires that 'source' be unique)
+counter = Counter([s['source'].strip() for s in data])
+counter = {k:v for k,v in counter.items() if v > 1}  # select duplicates
+data = [d for d in data if d['source'].strip() not in counter.keys()]
+
+db.Sources.insert().execute(data)
+db.query(db.Sources).count()
+
+# ----------------------------------------------------------------------------------------
+# Names table
+# TODO: This needs some further work
+name_data = []
+for row in temp:
+    if row.designation not in [s['source'] for s in data]: continue
+    name_list = set([row.designation] + [s.strip() for s in row.names.split(',')])
+    for n in name_list:
+        name_data.append({'source': row.designation, 'other_name': n})
+
+db.Names.insert().execute(name_data)
+db.query(db.Names).count()
