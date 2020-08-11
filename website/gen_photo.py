2823d11c05eb98fde17be89695645f38f1160e4a
Will-Cooper <w.cooper@herts.ac.uk>
2020-08-11 12:28:48 +0100

    Move and document, minor bug fixes, html resources localised

--- website/gen_photo.py
+++ website/gen_photo.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 """
 Creates photometry table
+
+Attributes
+----------
+KNOWN_MAGS
+    Dictionary of photometric bands against central wavelength in microns
+
+Methods
+-------
+photometry_exists
+    Grabs photometry from object inventory on query
+main
+    Queries database for given object and finds photometry if it exists
 """
 # local files
 from web_base import load_db, target_exists, target_argparse
 
 # global vars
+# TODO: expand known mags or use different method of initialising tables depending on if plotting photometry
 KNOWN_MAGS = {'WISE_W1': 3.368, 'WISE_W2': 4.618, 'WISE_W3': 12.082, 'WISE_W4': 22.194}  # central wave in microns
 
 
@@ -25,10 +38,11 @@ def photometry_exists(inventory: dict):
             raise TypeError('Photometry entry needs to produce list')
     finally:
         photo = inventory['Photometry']
-    # photo = [band for band in photo if band['band'] in KNOWN_MAGS.keys()]
-    bands = [band['band'] for band in photo]
-    mags = [band['magnitude'] for band in photo]
-    errs = [band['magnitude_error'] for band in photo]
+    bands, mags, errs = [], [], []
+    for band in photo:
+        bands.append(band['band'])  # photometric band name
+        mags.append(band['magnitude'])  # magnitude for object
+        errs.append(band['magnitude_error'])  # magnitude error for object
     return bands, mags, errs
 
 
@@ -41,12 +55,12 @@ def main(target: str):
     target
         Name of the target to be searched for, given as script argument
     """
-    db = load_db()
-    inventory = target_exists(db, target)
-    bands, mags, errs = photometry_exists(inventory)
+    db = load_db()  # load simple database
+    inventory = target_exists(db, target)  # query database for given object
+    bands, mags, errs = photometry_exists(inventory)  # get lists for photometry
     return bands, mags, errs
 
 
-if __name__ == '__main__':
-    _tgt = target_argparse()
+if __name__ == '__main__' or 'bokeh' in __name__:
+    _tgt = target_argparse()  # pull target name from system arguments
     main(_tgt)
--- website/gen_plot.py
+++ website/gen_plot.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 """
 Generates the script and div for the bokeh plot of spectra
+
+Methods
+-------
+spectra_exists
+    Takes spectra from database for given object
+main
+    Queries database for given object and finds spectra
 """
 # local files
 from web_base import load_db, target_exists, target_argparse
@@ -38,12 +45,12 @@ def main(target: str):
     target
         Name of the target to be searched for, given as script argument
     """
-    db = load_db()
-    inventory = target_exists(db, target)
-    spectra = spectra_exists(inventory)
+    db = load_db()  # load simple database
+    inventory = target_exists(db, target)  # query database for given object
+    spectra = spectra_exists(inventory)  # pull spectra from database query
     return spectra
 
 
-if __name__ == '__main__':
-    _tgt = target_argparse()
+if __name__ == '__main__' or 'bokeh' in __name__:
+    _tgt = target_argparse()  # pull target name from system arguments
     main(_tgt)
