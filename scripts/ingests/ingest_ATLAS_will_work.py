d204a60605e1ee37cd38aa78160c46dd1e5d63f7
Ella Hort <ebhort27@gmail.com>
2021-02-23 16:35:47 -0500

    removed unneeded lines

--- scripts/ingests/ingest_ATLAS_will_work.py
+++ scripts/ingests/ingest_ATLAS_will_work.py
@@ -17,61 +17,14 @@ warnings.filterwarnings("ignore", module='astroquery.simbad')
 connection_string = 'sqlite:///../../SIMPLE.db'  # SQLite
 create_database(connection_string)
 db = Database(connection_string)
-#db.load_database('../../data')
+db.load_database('../../data')
 
 
 # load table of sources to ingest
-
-
-
 input_file = ("ATLAS_table.vot")
 ATLAS=Table.read(input_file)
 
-# Inital attempt, fixing object name flag issue with simbad
-
-'''
-#simbad search fix
-# PSO J318.5338-22.8603
-Ind = ATLAS['Name'] == 'PSO J318.5-22'
-ATLAS['Name'][Ind]='PSO J318.5338-22.8603'
-
-Ind = ATLAS['Name'] == 'Luh 16AB'
-ATLAS['Name'][Ind]='Luhman 16'
-
-Ind = ATLAS['Name'] == '2MASS J2339101+135230'
-ATLAS['Name'][Ind]='2MASSI J2339101+135230'
-
-Ind = ATLAS['Name'] == '2MASS J1110100+0116130'
-ATLAS['Name'][Ind]='2MASS J11101001+0116130'
-
-Ind = ATLAS['Name'] == '2MASS J2228288-4310262'
-ATLAS['Name'][Ind]='2MASS J22282889-4310262'
-
-Ind = ATLAS['Name'] == '2MASS J0817300-6155158'
-ATLAS['Name'][Ind]='2MASS J08173001-6155158'
-
-Ind = ATLAS['Name'] == 'S Ori 70'
-ATLAS['Name'][Ind]='[BZR99] S Ori 70'
-
-
-Ind = ATLAS['Name'] == 'S Ori 70'
-ATLAS['Name'][Ind]='[BZR99] S Ori 70'
-
-
-Ind = ATLAS['Name'] == 'S Ori 70'
-ATLAS['Name'][Ind]='[BZR99] S Ori 70'
-
-
-Ind = ATLAS['Name'] == 'S Ori 70'
-ATLAS['Name'][Ind]='[BZR99] S Ori 70'
-
-
-Ind = ATLAS['Name'] == 'S Ori 70'
-ATLAS['Name'][Ind]='[BZR99] S Ori 70'
-
-'''
-
-#attempt 2, fixing simbad flag issue:
+#Making sure all target names are simbad resolvable:
 
 resolved_name = []
 
