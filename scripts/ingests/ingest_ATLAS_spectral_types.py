0405ad1db59eed26c7b8b2dd654dd75a619eedb8
Ella Hort <ebhort27@gmail.com>
2021-05-04 17:15:08 -0400

    add inits.py and updates spectral type ingest

--- scripts/ingests/ingest_ATLAS_spectral_types.py
+++ scripts/ingests/ingest_ATLAS_spectral_types.py
@@ -2,26 +2,28 @@
 
 from astrodbkit2.astrodb import create_database
 from astrodbkit2.astrodb import Database
+#from simple.schema import *
 from astropy.table import Table
 import numpy as np
 import re
 from utils import convert_spt_string_to_code
 
-connection_string = 'sqlite:///../../SIMPLE.db'  # SQLite
-create_database(connection_string)
+connection_string = 'sqlite:///SIMPLE.db'  # SQLite
+#create_database(connection_string)
 db = Database(connection_string)
-db.load_database('../../data')
+#db.load_database('data')
 
+#Adding new reference Manj19 to publications table in database
+new_ref = [{'name': 'Manj19'}]
+#db.Publications.insert().execute(new_ref)
 
 # load table of sources to ingest
-ingest_table = Table.read("ATLAS_table.vot")
+ingest_table = Table.read("scripts/ingests/ATLAS_table.vot")
 ingest_table_df = ingest_table.to_pandas()
 names = ingest_table['Name']
 n_sources = len(names)
-# regime = ['infrared'] * n_sources
 spectral_types_unknown = ingest_table['SpType'] # pre-existing spectral types
 spectral_types_spex = ingest_table['SpTSpeX'] # new spectral types
-# spt_refs = ingest_table['spt_ref']
 
 # fetch primary name identifier from database
 db_names = []
@@ -43,19 +45,27 @@ regime = ['nir']*len(db_names_spex)
 spt_ref = ['Manj19']*len(db_names_spex)
 SpT_table_spex = Table([db_names_spex, spex_types_string, spex_types_codes, regime, spt_ref], names=('source','spectral_type_string','spectral_type_code','regime','reference'))
 SpT_table_spex_df = SpT_table_spex.to_pandas() # make a Pandas dataframe to explore  with Pycharm
+#print(SpT_table_spex_df)
+db.add_table_data(SpT_table_spex, table='SpectralTypes', fmt='astropy')
 
 # Find out which sources don't have spectral types
-# needs_spectral_type = []
-# spectral_types_to_add = []
-# for i, db_name in enumerate(db_names):
-# 	db_spectral_types = db.query(db.SpectralTypes).filter(db.SpectralTypes.c.source == db_name).table()
-# 	if db_spectral_types is None or len(db_spectral_types) == 0:
-# 		needs_spectral_type.append(db_name)
-# 		spectral_types_to_add.append(spectral_types_optical[i])
+db_names_needs_spectral_type = []
+spectral_types_to_add = []
+for i, db_name in enumerate(db_names):
+ 	db_spectral_types = db.query(db.SpectralTypes).filter(db.SpectralTypes.c.source == db_name).table()
+ 	if db_spectral_types is None or len(db_spectral_types) == 0:
+ 		db_names_needs_spectral_type.append(db_name)
+ 		spectral_types_to_add.append(spectral_types_unknown[i])
 
-# Convert SpT string to code
-#spectral_type_codes_optical = convert_spt_string_to_code(spectral_types_to_add, verbose = True)
-#spectral_type_codes_nir = convert_spt_string_to_code(spectral_types_nir, verbose = True)
+#Convert SpT string to code
+spectral_type_codes_unknown = convert_spt_string_to_code(spectral_types_to_add, verbose = True)
+regime = ['unknown']*len(db_names_needs_spectral_type)
+spt_ref = ['Missing']*len(db_names_needs_spectral_type)
+comments = ['From ATLAS Table Manjavacas etal. 2019']*len(db_names_needs_spectral_type)
+SpT_table_unknown = Table([db_names_needs_spectral_type, spectral_types_to_add, spectral_type_codes_unknown, regime, spt_ref, comments],
+	names=('source','spectral_type_string','spectral_type_code','regime','reference', 'comments'))
 
-# Comment = From ATLAS Table Manajavacas etal. 2019
-# reference = UNKNOWN
+#Print out the results
+#print(SpT_table_unknown)
+db.add_table_data(SpT_table_unknown, table='SpectralTypes', fmt='astropy')
+db.save_db('data')
