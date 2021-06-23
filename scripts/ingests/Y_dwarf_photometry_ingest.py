8a255c455f6e8ccbe9929eb00613bd09c98def40
Ella Hort <ebhort27@gmail.com>
2021-06-23 12:53:31 -0400

    debugged and cleaned up ingest script, moved ingest function to utils

--- scripts/ingests/Y_dwarf_photometry_ingest.py
+++ scripts/ingests/Y_dwarf_photometry_ingest.py
@@ -1,56 +1,13 @@
 import sys
+from sqlalchemy.sql.elements import Null
 sys.path.append('.')
 from astrodbkit2.astrodb import create_database
 from astrodbkit2.astrodb import Database
 from simple.schema import *
-#from utils import ingest_photometry
+from utils import ingest_photometry
 from astropy.table import Table
 from pathlib import Path
 import os 
-
-#def ingest_photometry adapted from ingest_pm and ingest_parallaxes from utils 
-#NOTE: Have to eventually add back in epoch, band, and ucd below
-def ingest_photometry(db, sources, ch1mag, ch1mag_err, ch2mag, ch2mag_err, telescope, instrument, epoch, reference, verbose=False):
-    verboseprint = print if verbose else lambda *a, **k: None
-
-    n_added = 0
-
-    for i, source in enumerate(sources):
-        db_name = db.search_object(source, output_table='Sources')[0]['source']
-        # Search for existing proper motion data and determine if this is the best
-        adopted = None
-        source_photometry_data = db.query(db.Photometry).filter(db.Photometry.c.source == db_name).table()
-        if source_photometry_data is None or len(source_photometry_data) == 0:
-            adopted = True
-        else:
-            print("OTHER PHOTOMETRY MOTION EXISTS: ",source_photometry_data)
-
-        # TODO: Work out logic for updating/setting adopted. Be it's own function.
-
-        # TODO: Make function which validates refs
-
-        # Construct data to be added
-        photometry_data = [{'source': db_name,
-                          #'band': band[i],
-                          #'ucd' : ucd[i],
-                          'ch1mag' : ch1mag[i],
-                          'ch1mag_err' : ch1mag_err[i],
-                          'ch2mag': ch2mag[i],
-                          'ch2mag_err': ch2mag_err[i],
-                          'telescope': telescope[i],
-                          'instrument': instrument[i],
-                          'epoch': epoch[i], 
-                          'adopted': adopted,
-                          'reference': reference[i]}]
-        verboseprint('Photometry data: ',photometry_data)
-
-        # Consider making this optional or a key to only view the output but not do the operation.
-        db.Photometry.insert().execute(photometry_data)
-        n_added += 1
-
-    print("Added to database: ", n_added)
-
-
 
 
 
@@ -75,27 +32,36 @@ if not db_file_path.exists():
 else:
     db = Database(db_connection_string) #if database already exists, connects to .db file
 
+#Adding Spizter and IRAC to telescope and instrument tables
+new_telescope=[{'name':'Spitzer'}]
+db.Telescopes.insert().execute(new_telescope)
+
+new_instrument=[{'name':'IRAC'}]
+db.Instruments.insert().execute(new_instrument)
+print('done')
+
 # load table
 ingest_table = Table.read('scripts/ingests/Y-dwarf_table.csv', data_start=2)
 
+#Defining variables
 source = ingest_table['source']
-#band = ingest_table['band']
-#Don't know what ucd is or if it is needed
-#ucd = ingest_table['ucd']
+band1 = ['IRAC.I1']*len(source)
+band2 = ['IRAC.I2']*len(source)
+ucd1 = ['em.IR.3-4um']*len(source)
+ucd2 = ['em.IR.4-8um']*len(source)
 ch1mag = ingest_table['ch1_mag']
 ch1mag_err = ingest_table['ch1_err']
 ch2mag = ingest_table['ch2_mag']
 ch2mag_err = ingest_table['ch2_err']
-telescope = ['Spizter']*len(ch1mag)
-instrument = ['IRAC']*len(ch1mag)
-#Leaving epoch blank for now since we don't know if the epoch table in the csv files corresponds to pm epoch or photometry
-epoch = [0]*len(ch1mag)
+telescope = ['Spitzer']*len(source)
+instrument = ['IRAC']*len(source)
 reference = ingest_table['Spizter_ref']
 
-#ingest_photometry defintion in utils.py, adapted from ingest_parallaxes definition
-#NOTE: have to eventually add back in ucd, band, and epoch
-ingest_photometry(db, source, ch1mag, ch1mag_err, ch2mag, ch2mag_err, telescope, instrument, epoch, reference, verbose=True)
+#ingesting band 1 photometry
+ingest_photometry(db, sources = source, bands = band1, ucds = ucd1, magnitudes = ch1mag, magnitude_errors = ch1mag_err, telescope = telescope, instrument = instrument, reference = reference, verbose=True)
 
+#ingesting band 2 photometry 
+ingest_photometry(db, sources = source, bands = band2, ucds = ucd2, magnitudes = ch2mag, magnitude_errors = ch2mag_err, telescope = telescope, instrument = instrument, reference = reference, verbose=True)
 
 if not DRY_RUN:
     db.save_db('data') #edits the JSON files if we're not doing a dry run
