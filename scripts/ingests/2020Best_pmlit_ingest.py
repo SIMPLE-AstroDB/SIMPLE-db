f186e2fd0e20836f73b60d6e250f554b9bb09c46
Ella Hort <ebhort27@gmail.com>
2021-07-28 16:00:55 -0400

    started reassigning mismatched pm refs

--- scripts/ingests/2020Best_pmlit_ingest.py
+++ scripts/ingests/2020Best_pmlit_ingest.py
@@ -63,9 +63,61 @@ ref_pm_lit = ingest_table['ref_pm_lit']
 #ingest_table_df = pd.DataFrame({'sources': sources, 'pm_ra' : ra_UKIRT, 'pm_ra_err' : ra_UKIRT_err, 'pm_dec' : dec_UKIRT, 'pm_dec_err' : dec_UKIRT_err, 'pm_ref' : ref_pm_UKIRT})
 
 
+
 df = pd.read_csv('scripts/ingests/UltracoolSheet-Main.csv', usecols=['name' ,'pmra_lit', 'pmraerr_lit', 'pmdec_lit', 'pmdecerr_lit', 'ref_pm_lit']) .dropna()
 df.reset_index(inplace=True, drop=True)
+
+for i, ref in enumerate(df.ref_pm_lit):
+    if ref == 'Dupu12':
+        df.ref_pm_lit[i] = 'Dupu12a'
+    if ref == 'VanL07':
+        df.ref_pm_lit[i] = 'vanL07'
+    if ref == 'Kend07a':
+        df.ref_pm_lit[i] = 'Kend07'
+    if ref == 'Lepi02b':
+        df.ref_pm_lit[i] = 'Lepi02'
+    if ref == "Mace13a":
+        df.ref_pm_lit[i] = "Mace13"
+    if ref == 'Kend03a':
+        df.ref_pm_lit[i] = "Kend03"
+    if ref == 'West08a':
+        df.ref_pm_lit[i] = "West08"
+    if ref == 'Reid05b':
+        df.ref_pm_lit[i] = "Reid05"
+    if ref == 'Burg08b':
+        df.ref_pm_lit[i] = "Burg08c"
+    if ref == 'Burg08c':
+        df.ref_pm_lit[i] = "Burg08d"
+    if ref == 'Burg08d':
+        df.ref_pm_lit[i] = "Burg08b"
+    if ref == 'Gagn15b':
+        df.ref_pm_lit[i] = "Gagn15c"
+    if ref == 'Gagn15c':
+        df.ref_pm_lit[i] = "Gagn15b"
+    if ref == 'Lodi07a':
+        df.ref_pm_lit[i] = "Lodi07b"
+    if ref == 'Lodi07b':
+        df.ref_pm_lit[i] = "Lodi07a"
+    if ref == 'Reid02c':
+        df.ref_pm_lit[i] = "Reid02b"
+    if ref == 'Reid06a':
+        df.ref_pm_lit[i] = "Reid06b"
+    if ref == 'Reid06b':
+        df.ref_pm_lit[i] = "Reid06a"
+    if ref == 'Scho04b':
+        df.ref_pm_lit[i] = "Scho04a"
+    if ref == 'Scho10a':
+        df.ref_pm_lit[i] = "Scho10b"
+    if ref == 'Tinn93b':
+        df.ref_pm_lit[i] = "Tinn93c"
+    if ref == 'Lieb79f':
+        df.ref_pm_lit[i] = "Lieb79"
+    if ref == 'Prob83c':
+        df.ref_pm_lit[i] = "Prob83"
+    if ref == 'Jame08a':
+        df.ref_pm_lit[i] = 'Jame08'
 print(df)
+
 
 
 
