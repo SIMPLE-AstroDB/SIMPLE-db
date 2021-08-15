c9fa1e34184ec8c004d753e08471c9049f861a09
David Rodriguez <drodriguez@stsci.edu>
2021-08-15 17:36:03 -0400

    updating database generation to allow for either sqlite or postgres database

--- scripts/tutorials/generate_database.py
+++ scripts/tutorials/generate_database.py
@@ -9,26 +9,19 @@ from simple.schema import *
 
 # Location of source data
 DB_PATH = 'data'
-# Postgres settings
-# DB_TYPE = 'postgres'
-# DB_NAME = 'localhost/SIMPLE'
-# SQLlite settings
-DB_TYPE = 'sqlite'
 DB_NAME = 'SIMPLE.db'
 
 # Used to overwrite AstrodbKit2 reference tables defaults
 REFERENCE_TABLES = ['Publications', 'Telescopes', 'Instruments', 'Modes', 'PhotometryFilters']
 
-# Set correct connection string
-if DB_TYPE == 'sqlite':
-    # First, remove the existing database in order to recreate it from the schema
-    # If the schema has not changed, this part can be skipped
-    if os.path.exists(DB_NAME):
-        os.remove(DB_NAME)
-    connection_string = 'sqlite:///' + DB_NAME
-elif DB_TYPE == 'postgres':
+
+def load_postgres(connection_string):
     # For Postgres, we connect and drop all database tables
-    connection_string = 'postgresql://' + DB_NAME
+
+    # Fix for SQLAlchemy 1.4.x
+    if connection_string.startswith("postgres://"):
+        connection_string = connection_string.replace("postgres://", "postgresql://", 1)
+
     try:
         db = Database(connection_string)
         db.base.metadata.drop_all()
@@ -38,14 +31,48 @@ elif DB_TYPE == 'postgres':
         # Database already empty or doesn't yet exist
         pass
 
-create_database(connection_string)
+    # Proceed to load the database
+    load_database(connection_string)
 
-# Now that the database is created, connect to it and load up the JSON data
-db = Database(connection_string, reference_tables=REFERENCE_TABLES)
-db.load_database(DB_PATH, verbose=False)
 
-print('New database generated.')
+def load_sqlite():
+    # First, remove the existing database in order to recreate it from the schema
+    # If the schema has not changed, this part can be skipped
+    if os.path.exists(DB_NAME):
+        os.remove(DB_NAME)
+    connection_string = 'sqlite:///' + DB_NAME
 
-# Close all connections
-db.session.close()
-db.engine.dispose()
+    # Proceed to load the database
+    load_database(connection_string)
+
+
+def load_database(connection_string):
+    # Create and load the database
+    create_database(connection_string)
+
+    # Now that the database is created, connect to it and load up the JSON data
+    db = Database(connection_string, reference_tables=REFERENCE_TABLES)
+    db.load_database(DB_PATH, verbose=False)
+
+    print('New database generated.')
+
+    # Close all connections
+    db.session.close()
+    db.engine.dispose()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        architecture = sys.argv[1].lower()
+        if architecture == 'postgres':
+            if len(sys.argv) > 2:
+                connection_string = sys.argv[2]
+            else:
+                connection_string = os.getenv('SIMPLE_DATABASE_URL')
+            load_postgres(connection_string)
+        elif architecture == 'sqlite':
+            load_sqlite()
+        else:
+            print(f'Unrecognized architecture: {architecture}')
+    else:
+        print('Specify database architecture (eg, postgres, sqlite)')
