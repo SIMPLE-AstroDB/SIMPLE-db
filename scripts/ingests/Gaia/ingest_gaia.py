f180d093587b76d5cde7c0d4dba19c033078dced
kelle <kellecruz@gmail.com>
2021-09-08 11:27:02 -0400

    removing unused bits, clean up code

--- scripts/ingests/Gaia/ingest_gaia.py
+++ scripts/ingests/Gaia/ingest_gaia.py
@@ -24,7 +24,6 @@ def load_db():
     db_file = 'SIMPLE.db'
     db_file_path = Path(db_file)
     db_connection_string = 'sqlite:///SIMPLE.db'
-      # SQLite browser
 
     if RECREATE_DB and db_file_path.exists():
         os.remove(db_file)  # removes the current .db file if one already exists
@@ -46,28 +45,29 @@ db = load_db()
 gaia_sources = db.query(db.Names.c.source).filter(db.Names.c.other_name.ilike("Gaia DR2%")).table()
 if gaia_sources:
     gaia_sources_list = gaia_sources['source'].tolist()
-    no_gaia_source_id = db.query(db.Sources.c.source).filter(db.Sources.c.source.notin_(gaia_sources_list)).table()['source'].tolist()
+    no_gaia_source_id = db.query(db.Sources.c.source).filter(db.Sources.c.source.notin_(gaia_sources_list)).\
+        table()['source'].tolist()
 else:
     no_gaia_source_id = db.query(db.Sources.c.source).table()['source'].tolist()
 
 
 # Use SIMBAD to find Gaia designations for sources without a Gaia designation
-def find_gaia_in_simbad(no_gaia_source_id, verbose = False):
+def find_gaia_in_simbad(no_gaia_source_id):
     verboseprint = print if VERBOSE else lambda *a, **k: None
 
-    sources=no_gaia_source_id
+    sources = no_gaia_source_id
     n_sources = len(sources)
 
     Simbad.reset_votable_fields()
-    Simbad.add_votable_fields('typed_id') # keep search term in result table
-    Simbad.add_votable_fields('ids') # add all SIMBAD identifiers as an output column
+    Simbad.add_votable_fields('typed_id')  # keep search term in result table
+    Simbad.add_votable_fields('ids')  # add all SIMBAD identifiers as an output column
     print("simbad query started")
     result_table = Simbad.query_objects(sources)
     print("simbad query ended")
 
-    ind = result_table['SCRIPT_NUMBER_ID'] > 0 # find indexes which contain results
+    ind = result_table['SCRIPT_NUMBER_ID'] > 0  # find indexes which contain results
 
-    simbad_ids = result_table['TYPED_ID','IDS'][ind] #.topandas()
+    simbad_ids = result_table['TYPED_ID', 'IDS'][ind]  # .topandas()
 
     db_names = []
     simbad_gaia_designation = []
@@ -78,7 +78,7 @@ def find_gaia_in_simbad(no_gaia_source_id, verbose = False):
         gaia_designation = [i for i in ids if "Gaia DR2" in i]
 
         if gaia_designation:
-            verboseprint(db_name,gaia_designation[0])
+            verboseprint(db_name, gaia_designation[0])
             db_names.append(db_name)
             simbad_gaia_designation.append(gaia_designation[0])
 
@@ -93,7 +93,7 @@ def find_gaia_in_simbad(no_gaia_source_id, verbose = False):
 
     table.write('scripts/ingests/Gaia/gaia_designations.xml', format='votable', overwrite=True)
 
-    return(table)
+    return table
 
 
 # gaia_designations = find_gaia_in_simbad(no_gaia_source_id)
@@ -101,7 +101,8 @@ def find_gaia_in_simbad(no_gaia_source_id, verbose = False):
 
 def query_gaia():
     gaia_query_string = "SELECT *,upload_table.db_names FROM gaiadr2.gaia_source " \
-                             "INNER JOIN tap_upload.upload_table ON gaiadr2.gaia_source.designation = tap_upload.upload_table.gaia_designation  "
+                             "INNER JOIN tap_upload.upload_table ON " \
+                        "gaiadr2.gaia_source.designation = tap_upload.upload_table.gaia_designation  "
     job_gaia_query = Gaia.launch_job(gaia_query_string, upload_resource='scripts/ingests/Gaia/gaia_designations.xml',
                                      upload_table_name="upload_table", verbose=True)
 
@@ -112,8 +113,9 @@ def query_gaia():
 # Re-run Gaia query
 # gaia_data = query_gaia()
 
+
 # read results from saved table
-gaia_data = Table.read('scripts/ingests/Gaia/gaia_data.xml',format='votable')
+gaia_data = Table.read('scripts/ingests/Gaia/gaia_data.xml', format='votable')
 
 
 # add Gaia telescope, instrument, and Gaia filters
@@ -151,13 +153,13 @@ def update_ref_tables(db):
     db.save_reference_table('PhotometryFilters', 'data')
 
 
-# TODO: add Gaia designations to Names table
+# add Gaia designations to Names table
 add_names(db, sources=gaia_data['db_names'], other_names=gaia_data['gaia_designation'], verbose=True)
 
-# TODO: ingest Gaia proper motions
 
+# add Gaia proper motions
 def add_gaia_pms(db):
-    gaia_pms_df = gaia_data['db_names','pmra', 'pmra_error', 'pmdec', 'pmdec_error'].to_pandas()
+    gaia_pms_df = gaia_data['db_names', 'pmra', 'pmra_error', 'pmdec', 'pmdec_error'].to_pandas()
     gaia_pms_df = gaia_pms_df[gaia_pms_df['pmra'].notna()]
     gaia_pms_df.reset_index(inplace=True, drop=True)
     refs = ['GaiaDR2'] * len(gaia_pms_df)
@@ -166,8 +168,11 @@ def add_gaia_pms(db):
                           gaia_pms_df['pmdec'], gaia_pms_df['pmdec_error'],
                           refs)
 
+
 add_gaia_pms(db)
 
+
+# add Gaia parallaxes
 def add_gaia_parallaxes(db):
     unmasked = np.logical_not(gaia_data['parallax'].mask).nonzero()
     gaia_parallaxes = gaia_data[unmasked]['db_names', 'parallax', 'parallax_error']
@@ -176,75 +181,7 @@ def add_gaia_parallaxes(db):
                       gaia_parallaxes['parallax_error'], refs, verbose=True)
 
 
+add_gaia_parallaxes(db)
+
+
 # TODO: ingest Gaia photometry
-
-
-
-
-
-# MAY NOT USE THIS
-def find_tmass_gaia_matches():
-    # Find all sources in database with 2MASS designations
-    tmass_sources = db.query(db.Names).filter(db.Names.c.other_name.like('2MASS J%')).all()
-    n_tmass = len(tmass_sources)
-
-    # only do a small subset
-    tmass_sources = tmass_sources[0:100]
-
-    # should only do unique 2MASS names
-    # tmass_sources = unique(tmass_sources,keys='source')
-
-    # make table of sources in database with 2MASS designations
-    db_names = []
-    tmass_source_ids = []
-    for tmass_source in tmass_sources:
-        db_names.append(tmass_source[0])
-        # remove "2MASS J" to match with Gaia catalog id
-        tmass_source_ids.append(tmass_source[1][7:23])
-
-    table = Table(tmass_source,db_names,colnames=('',''))
-    table.write('table.xml', overwrite=True)
-
-    # Query Gaia catalog to get info on each 2MASS source
-    i=0.
-    for db_name, tmass_source in zip(db_names,tmass_source_ids):
-        # query 2MASS and DR2 at the same time
-        tmass_query_string = "SELECT *,upload_table.db_name FROM gaiadr2.gaia_source " \
-                         "INNER JOIN gaiadr2.tmass_best_neighbour ON gaiadr2.gaia_source.source_id = gaiadr2.tmass_best_neighbour.source_id " \
-                         "WHERE gaiadr2.tmass_best_neighbour.original_ext_source_id = '"+ tmass_source + "'"  # ('03552337+1133437','03550477-1032415')" #tap_upload.upload_table"
-        #"WHERE gaiadr2.tmass_best_neighbour.original_ext_source_id IN (SELECT ?name? from upload_table)"
-        # INNER JOIN tap_upload.upload_table ON  gaiadr2.tmass_best_neighbour.original_ext_source_id = tap_upload.upload_table.name
-        job_tmass = Gaia.launch_job(tmass_query_string) # upload_resource='upload.xml', upload_table_name="upload_table", verbose=True)
-
-        result = job_tmass.get_results()
-
-        if len(result) > 0:
-            # add database source name to results table
-            result.add_column(db_name, name='SIMPLE_source', index=0)
-
-            if i == 0:
-                all_results = result
-                i = 1
-            else:
-                all_results.add_row(result[0])
-
-        out_file ='scripts/ingests/Gaia/tmass_gaia_results_test.xml'
-        all_results.write(out_file, format='votable', overwrite=True)
-        n_matches = len(all_results)
-        print('Found', n_matches, 'Gaia sources for', n_tmass, ' 2MASS sources')
-        print('Wrote table to ',out_file)
-        return all_results
-
-
-# Of those without Gaia designation, find 2MASS and WISE designations.
-# tmass_sources = db.query(db.Names.c.other_name).filter(and_(db.Names.c.other_name.ilike("2MASS J%"),
-#                                                        db.Names.c.source.in_(no_gaia_source_id))).table()
-
-
-# read results from saved table
-# tmass_matches = Table.read('scripts/ingests/Gaia/tmass_gaia_results.xml',format='votable')
-# print("tmass matches ", len(tmass_matches))
-#print(tmass_matches['SIMPLE_source','designation','pmra','parallax','phot_rp_mean_mag'])
-
-
-
