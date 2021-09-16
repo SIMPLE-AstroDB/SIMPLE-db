9955d2d463596c2dfed71ceacad5497792f9e54f
kelle <kellecruz@gmail.com>
2021-09-15 20:14:59 -0400

    finalized ingest script for first Gaia ingestion

--- scripts/ingests/Gaia/ingest_gaia.py
+++ scripts/ingests/Gaia/ingest_gaia.py
@@ -1,47 +1,18 @@
-import os
-from astrodbkit2.astrodb import create_database
-from astrodbkit2.astrodb import Database
 from scripts.ingests.utils import *
-from simple.schema import *
-from pathlib import Path
 from astroquery.gaia import Gaia
-from sqlalchemy import and_, or_
-from astropy.table import Table, Column, unique
-from astropy.io.votable import from_table, writeto
 from astroquery.simbad import Simbad
-from astroquery.vizier import Vizier
-import astropy.units as u
+from astropy.table import Table
 import numpy as np
+from sqlalchemy import func
 
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
-RECREATE_DB = False  # recreates the .db file from the data files
-VERBOSE = True
+RECREATE_DB = True  # recreates the .db file from the data files
+VERBOSE = False
 
 verboseprint = print if VERBOSE else lambda *a, **k: None
 
-
-def load_db():
-    # Utility function to load the database
-
-    db_file = 'SIMPLE.db'
-    db_file_path = Path(db_file)
-    db_connection_string = 'sqlite:///SIMPLE.db'
-
-    if RECREATE_DB and db_file_path.exists():
-        os.remove(db_file)  # removes the current .db file if one already exists
-
-    if not db_file_path.exists():
-        create_database(db_connection_string)  # creates empty database based on the simple schema
-        db = Database(db_connection_string)  # connects to the empty database
-        db.load_database('data/')  # loads the data from the data files into the database
-    else:
-        db = Database(db_connection_string)  # if database already exists, connects to .db file
-
-    return db
-
-
-db = load_db()
+db = load_simpledb(RECREATE_DB=RECREATE_DB)
 
 # get all sources that have Gaia DR2 designations
 # 2nd query which is all sources that are not in that list
@@ -107,7 +78,7 @@ def query_gaia():
                              "INNER JOIN tap_upload.upload_table ON " \
                         "gaiadr2.gaia_source.designation = tap_upload.upload_table.gaia_designation  "
     job_gaia_query = Gaia.launch_job(gaia_query_string, upload_resource='scripts/ingests/Gaia/gaia_designations.xml',
-                                     upload_table_name="upload_table", verbose=True)
+                                     upload_table_name="upload_table", verbose=VERBOSE)
 
     gaia_data = job_gaia_query.get_results()
 
@@ -122,7 +93,7 @@ gaia_data = Table.read('scripts/ingests/Gaia/gaia_data.xml', format='votable')
 
 
 # add Gaia telescope, instrument, and Gaia filters
-def update_ref_tables(db):
+def update_ref_tables():
     add_publication(db, doi='10.1051/0004-6361/201629272', name='Gaia')
     db.Publications.delete().where(db.Publications.c.name == 'GaiaDR2').execute()
     db.Publications.update().where(db.Publications.c.name == 'Gaia18').values(name='GaiaDR2').execute()
@@ -155,15 +126,16 @@ def update_ref_tables(db):
     db.save_reference_table('Telescopes', 'data')
     db.save_reference_table('PhotometryFilters', 'data')
 
-# update_ref_tables(db)
+# update_ref_tables()
 
 # add Gaia designations to Names table
-# add_names(db, sources=gaia_data['db_names'], other_names=gaia_data['gaia_designation'], verbose=True)
+add_names(db, sources=gaia_data['db_names'], other_names=gaia_data['gaia_designation'], verbose=VERBOSE)
 
 
 # add Gaia proper motions
-def add_gaia_pms(db):
+def add_gaia_pms():
     gaia_pms_df = gaia_data['db_names', 'pmra', 'pmra_error', 'pmdec', 'pmdec_error'].to_pandas()
+    # drop empty rows using Pandas
     gaia_pms_df = gaia_pms_df[gaia_pms_df['pmra'].notna()]
     gaia_pms_df.reset_index(inplace=True, drop=True)
     refs = ['GaiaDR2'] * len(gaia_pms_df)
@@ -173,40 +145,69 @@ def add_gaia_pms(db):
                           refs)
 
 
-# add_gaia_pms(db)
+add_gaia_pms()
 
 
 # add Gaia parallaxes
-def add_gaia_parallaxes(db):
-    unmasked = np.logical_not(gaia_data['parallax'].mask).nonzero()
-    gaia_parallaxes = gaia_data[unmasked]['db_names', 'parallax', 'parallax_error']
+def add_gaia_parallaxes():
+    # drop empty rows using Astropy Tables
+    unmasked_pi = np.logical_not(gaia_data['parallax'].mask).nonzero()
+    gaia_parallaxes = gaia_data[unmasked_pi]['db_names', 'parallax', 'parallax_error']
     refs = ['GaiaDR2'] * len(gaia_parallaxes)
     ingest_parallaxes(db, gaia_parallaxes['db_names'], gaia_parallaxes['parallax'],
-                      gaia_parallaxes['parallax_error'], refs, verbose=True)
+                      gaia_parallaxes['parallax_error'], refs, verbose=VERBOSE)
 
 
-# add_gaia_parallaxes(db)
-
-gaia_g_phot = gaia_data['gaia_designation','db_names', 'phot_g_mean_mag']
-gaia_rp_phot = gaia_data['db_names', 'phot_rp_mean_mag']
-
-# e_Gmag=abs(-2.5/ln(10)*e_FG/FG) from Vizier Note 37 on Gaia DR2 (I/345/gaia2)
-gaia_g_phot['g_unc'] = np.abs(-2.5/np.log(10)*gaia_data['phot_g_mean_flux_error']/gaia_data['phot_g_mean_flux'])
-gaia_rp_phot['rp_unc'] = np.abs(-2.5/np.log(10)*gaia_data['phot_rp_mean_flux_error']/gaia_data['phot_rp_mean_flux'])
-
-# TODO: drop nans using astropy Tables method.
+add_gaia_parallaxes()
 
 
-# TODO: ingest Gaia photometry
-def add_gaia_photometry(db):
+def add_gaia_photometry():
+    # drop empty rows
+    unmasked_gphot = np.logical_not(gaia_data['phot_g_mean_mag'].mask).nonzero()
+    gaia_g_phot = gaia_data[unmasked_gphot]['db_names', 'phot_g_mean_flux', 'phot_g_mean_flux_error',
+                                            'phot_g_mean_mag']
 
-    ingest_photometry(db, gaia_rp_phot['db_names'], 'GAIA2.Grp', gaia_rp_phot['phot_rp_mean_mag'], gaia_rp_phot['rp_unc'],
-                      'GaiaDR2', ucds='em.opt.R', telescope='Gaia', instrument='Gaia', verbose=True)
+    unmased_rpphot = np.logical_not(gaia_data['phot_rp_mean_mag'].mask).nonzero()
+    gaia_rp_phot = gaia_data[unmased_rpphot]['db_names', 'phot_rp_mean_flux', 'phot_rp_mean_flux_error',
+                                             'phot_rp_mean_mag']
+
+    # e_Gmag=abs(-2.5/ln(10)*e_FG/FG) from Vizier Note 37 on Gaia DR2 (I/345/gaia2)
+    gaia_g_phot['g_unc'] = np.abs(
+        -2.5 / np.log(10) * gaia_g_phot['phot_g_mean_flux_error'] / gaia_g_phot['phot_g_mean_flux'])
+    gaia_rp_phot['rp_unc'] = np.abs(
+        -2.5 / np.log(10) * gaia_rp_phot['phot_rp_mean_flux_error'] / gaia_rp_phot['phot_rp_mean_flux'])
+
+    ingest_photometry(db, gaia_rp_phot['db_names'], 'GAIA2.Grp', gaia_rp_phot['phot_rp_mean_mag'],
+                      gaia_rp_phot['rp_unc'], 'GaiaDR2', ucds='em.opt.R', telescope='Gaia', instrument='Gaia',
+                      verbose=VERBOSE)
 
     ingest_photometry(db, gaia_g_phot['db_names'], 'GAIA2.G', gaia_g_phot['phot_g_mean_mag'], gaia_g_phot['g_unc'],
-                      'GaiaDR2', ucds='em.opt', telescope='Gaia', instrument='Gaia', verbose=True)
+                      'GaiaDR2', ucds='em.opt', telescope='Gaia', instrument='Gaia', verbose=VERBOSE)
 
     return
 
+
+add_gaia_photometry()
+
+# query the database for number to add to the data tests
+# Expected numbers:
+#   Names added to database:  1266
+#   Proper motions added to database:  1076
+#   Parallaxes added to database:  1076
+#   Grp: Photometry measurements added to database:  1106
+#   G: Photometry measurements added to database:  1266
+
+phot_count = db.query(Photometry.band, func.count(Photometry.band)).group_by(Photometry.band).all()
+print('Photometry: ', phot_count)
+
+pi_count = db.query(Parallaxes.reference, func.count(Parallaxes.reference)).group_by(Parallaxes.reference).\
+    order_by(func.count(Parallaxes.reference).desc()).limit(10).all()
+print("Parallax count: ", pi_count)
+
+pm_count = db.query(ProperMotions.reference, func.count(ProperMotions.reference)).group_by(ProperMotions.reference).\
+    order_by(func.count(ProperMotions.reference).desc()).limit(20).all()
+print("Proper motion count: ", pm_count)
+
 # Write the JSON files
-# db.save_db(data/)
+if SAVE_DB:
+    db.save_database(directory='data/')
