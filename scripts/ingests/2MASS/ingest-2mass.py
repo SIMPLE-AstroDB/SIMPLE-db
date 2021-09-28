ad4d86194382d6f3eea08367fcd94b7255615698
kelle <kellecruz@gmail.com>
2021-09-28 14:49:52 -0400

    move functions into main script

--- scripts/ingests/2MASS/ingest-2mass.py
+++ scripts/ingests/2MASS/ingest-2mass.py
@@ -1,10 +1,7 @@
 from scripts.ingests.utils import *
 from scripts.ingests.ingest_utils import *
 from astropy.table import Table, setdiff
-from astropy import table
-import numpy as np
 from sqlalchemy import func
-import numpy as np
 
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
@@ -13,6 +10,33 @@ VERBOSE = True
 DATE_SUFFIX = 'Sep2021'
 
 db = load_simpledb(RECREATE_DB=RECREATE_DB)
+
+## FUNCITONS
+# QUERY SIMBAD TO GET 2MASS PHOTOMETRY
+def query_tmass(source_names):
+    Simbad.reset_votable_fields()
+    Simbad.add_votable_fields('typed_id')  # keep search term in result table
+    Simbad.add_votable_fields('flux(J)')
+    Simbad.add_votable_fields('flux_error(J)')
+    Simbad.add_votable_fields('flux_bibcode(J)')
+    Simbad.add_votable_fields('flux(H)')
+    Simbad.add_votable_fields('flux_error(H)')
+    Simbad.add_votable_fields('flux_bibcode(H)')
+    Simbad.add_votable_fields('flux(K)')
+    Simbad.add_votable_fields('flux_error(K)')
+    Simbad.add_votable_fields('flux_bibcode(K)')
+
+    print('2MASS query started')
+    result_table = Simbad.query_objects(source_names)
+    print('2MASS query complete')
+
+    # find indexes which contain 2MASS results
+    ind = result_table['FLUX_BIBCODE_J'] == '2003yCat.2246....0C'
+
+    tmass_phot = result_table['TYPED_ID', 'FLUX_J', 'FLUX_ERROR_J', 'FLUX_H','FLUX_ERROR_H', 'FLUX_K', 'FLUX_ERROR_K'][ind]
+    tmass_phot_unique = unique(tmass_phot, keys='TYPED_ID',keep='first')
+    print(len(tmass_phot), len(tmass_phot_unique))
+    return tmass_phot_unique
 
 #get list of all sources
 # sources = db.query(db.Sources.c.source).astropy()
@@ -36,8 +60,24 @@ tmass_phot_unique = unique(tmass_phot, keys='TYPED_ID',keep='first')
 # Find difference between all 2MASS and Names Table
 # add_names(db, sources=gaia_dr2_data['db_names'], other_names=gaia_dr2_data['gaia_designation'], verbose=VERBOSE)
 
+# ADD J band photometry
+unmasked_J_phot = np.logical_not(tmass_phot_unique['FLUX_J'].mask).nonzero()
+tmass_J_phot = tmass_phot_unique[unmasked_J_phot]['TYPED_ID', 'FLUX_J', 'FLUX_ERROR_J']
+ingest_photometry(db, tmass_J_phot['TYPED_ID'], '2MASS.J', tmass_J_phot['FLUX_J'], tmass_J_phot['FLUX_ERROR_J'],
+                  'Cutr03', ucds='em.IR.J', telescope='2MASS', instrument='2MASS')
 
-add_tmass_photometry(db, tmass_phot_unique)
+# ADD H band photometry
+unmasked_H_phot = np.logical_not(tmass_phot_unique['FLUX_H'].mask).nonzero()
+tmass_H_phot = tmass_phot_unique[unmasked_H_phot]['TYPED_ID', 'FLUX_H', 'FLUX_ERROR_H']
+tmass_H_phot.sort('FLUX_ERROR_H', reverse=True)
+ingest_photometry(db, tmass_H_phot['TYPED_ID'], '2MASS.H', tmass_H_phot['FLUX_H'], tmass_H_phot['FLUX_ERROR_H'],
+                  'Cutr03', ucds='em.IR.H', telescope='2MASS', instrument='2MASS')
+
+# ADD K band photometry
+unmased_K_phot = np.logical_not(tmass_phot_unique['FLUX_K'].mask).nonzero()
+tmass_K_phot = tmass_phot_unique[unmased_K_phot]['TYPED_ID', 'FLUX_K', 'FLUX_ERROR_K']
+ingest_photometry(db, tmass_K_phot['TYPED_ID'], '2MASS.Ks', tmass_K_phot['FLUX_K'], tmass_K_phot['FLUX_ERROR_K'],
+                  'Cutr03', ucds='em.IR.K', telescope='2MASS', instrument='2MASS')
 
 
 # ADded
