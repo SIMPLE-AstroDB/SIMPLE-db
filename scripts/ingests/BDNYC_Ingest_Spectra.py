0b38e264cf75407ca4f96f6218f6d483cf8f2bfe
Sherelyn Alejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2021-09-29 14:58:36 -0400

    Progress with Spectra Ingest Script

--- scripts/ingests/BDNYC_Ingest_Spectra.py
+++ scripts/ingests/BDNYC_Ingest_Spectra.py
@@ -1,15 +1,46 @@
 from scripts.ingests.utils import *
 import pandas as pd
 
-
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = False  # recreates the .db file from the data files
 VERBOSE = False
 
-
 db = load_simpledb('SIMPLE.db', RECREATE_DB=RECREATE_DB)
 
-#Read in CSV file with Pandas
-df = pd.read_csv('scripts/ingests/BDNYC_spectra.csv', usecols=['id', 'source_id', 'spectrum', 'wavelength_units', 'flux_units', 'wavelength_order', 'regime', 'publication_shortname', 'obs_date', 'filename', 'comments', 'best', 'local_spectrum', 'name', 'name', 'mode']) .dropna()
-df.reset_index(inplace=True, drop=True)
-print(len(df))
+# Read in CSV file with Pandas
+df = pd.read_csv('scripts/ingests/BDNYC_spectra.csv')
+data = Table.from_pandas(df)
+# Extracting Columns from CSV
+
+
+for row in data[0:10]:
+    Id = row['id']
+    source_id = row['source_id']
+    spectrum = row['spectrum']
+    wavelength_units = row["wavelength_units"]
+    flux_units = row["flux_units"]
+    wavelength_order = row["wavelength_order"]
+    regime = row["regime"]
+    publication_shortname = row["publication_shortname"]
+    obs_date = row["obs_date"]
+    comments = row["comments"]
+    local_spectrum = row["local_spectrum"]
+    telescope_name = row["name"]
+    instrument_name = row["name.1"]
+    mode = row["mode"]
+    row_data = [{'source': source_id,
+                 'spectrum': spectrum,
+                 'local_spectrum': local_spectrum,
+                 'regime': regime,
+                 'telescope': telescope_name,
+                 'instrument': instrument_name,
+                 'mode': mode,
+                 'observation_date': obs_date,
+                 'wavelength_units': wavelength_units,
+                 'flux_units': flux_units,
+                 'wavelength_order': wavelength_order,
+                 'comments': comments,
+                 'reference': publication_shortname}]
+    db.Spectra.insert().execute(row_data)
+
+
