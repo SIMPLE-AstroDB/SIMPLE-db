5c53c2cfb5802aa0d165e76eb5195b6f7dd3e605
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2021-11-12 21:25:52 -0500

    Working on fixing csv file to include discovery references.

--- scripts/ingests/Manja_ingest_spectra.py
+++ scripts/ingests/Manja_ingest_spectra.py
@@ -15,5 +15,10 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 # Read in CSV file as Astropy table
 data = Table.read('scripts/ingests/Manja_spectra.csv')
 
-ingest_sources(db, data['Source'], data['reference'], data['comments'])
+# ingest_publication(db, doi: str = None, bibcode: str = None, name: str = None, description: str = None,
+#                     ignore_ads: bool = False, save_db=False):
+
+# TODO: changes csv file to include all the discovery references
+# ingest_sources(db, data['Source'], data['reference'], comments=data['comments'])
 ingest_spectra(db, data['Source'], data['Spectrum'], 'nir', 'VLT', 'ISAAC', 'SW LRes', data['observation_date'], data['reference'], comments=data['comments'])
+
