ee57a0199b0f218be3b90bfde731ffc0769cede8
kelle <kellecruz@gmail.com>
2021-12-09 14:09:15 -0500

    full working version of ingest script.

--- scripts/ingests/Manja_ingest_spectra16.py
+++ scripts/ingests/Manja_ingest_spectra16.py
@@ -9,21 +9,14 @@ logger.setLevel(logging.INFO)
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 # Read in CSV file as Astropy table
-data = Table.read('scripts/ingests/Manja16_spectra6.csv')
+data = Table.read('scripts/ingests/Manja16 spectra - Sheet1.csv')
 
 ingest_instrument(db, telescope='ESO VLT', instrument='XShooter', mode='Echelle')
-# TODO: fix issue in ingest instrument function that is not adding the mode since it is likely only checkin the name
-# mode_add = [{'name': 'Echelle',
-#              'instrument': 'XShooter',
-#              'telescope': 'ESO VLT'}]
-# db.Modes.insert().execute(mode_add)
 
 # Add the sources to the database
+# ingest_sources(db, data['Source'])
 
-ingest_sources(db, data['Source'])
-# TODO: the ingest spectra function suspects that the other 21 spectra are duplicates. It is because it does not check the regime when determining a duplicate.
-
-# # Add the spectra to the database
+# Add the spectra to the database
 ingest_spectra(db, data['Source'], data['Spectrum'], data['regime'], 'ESO VLT', 'XShooter', 'Echelle',
                data['observation_date'],
                'Manj16', comments=data['spectrum comments'])
