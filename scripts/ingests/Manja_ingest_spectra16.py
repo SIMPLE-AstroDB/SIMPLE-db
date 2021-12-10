617deed576a4e2a8edaafef03be150bf57291fc4
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2021-12-09 21:50:27 -0500

    Updated the reference for SIMP J01365, and all the 42 spectra were added to the database successfully.

--- scripts/ingests/Manja_ingest_spectra16.py
+++ scripts/ingests/Manja_ingest_spectra16.py
@@ -9,12 +9,17 @@ logger.setLevel(logging.INFO)
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 # Read in CSV file as Astropy table
-data = Table.read('scripts/ingests/Manja16 spectra - Sheet1.csv')
+data = Table.read('scripts/ingests/Manja16_spectra7.csv')
 
 ingest_instrument(db, telescope='ESO VLT', instrument='XShooter', mode='Echelle')
 
 # Add the sources to the database
 # ingest_sources(db, data['Source'])
+
+simp = db.Sources.update()\
+         .where(db.Sources.c.source == 'SIMP J013656.5+093347.3')\
+         .values(reference='Arti06')
+db.engine.execute(simp)
 
 # Add the spectra to the database
 ingest_spectra(db, data['Source'], data['Spectrum'], data['regime'], 'ESO VLT', 'XShooter', 'Echelle',
