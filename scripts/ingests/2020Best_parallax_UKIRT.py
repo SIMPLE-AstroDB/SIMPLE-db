168ecc9fa2c125de01b89033cff35bf87844eb34
Ella Hort <ebhort27@gmail.com>
2021-12-14 13:55:53 -0800

    created new script for ingesting 2020 Best parallax UKIRT, seems to have added correctly

---
+++ scripts/ingests/2020Best_parallax_UKIRT.py
@@ -0,0 +1,25 @@
+import logging
+
+import pandas as pd
+from scripts.ingests.ingest_utils import ingest_parallaxes
+from scripts.ingests.utils import *
+
+SAVE_DB = False  # save the data files in addition to modifying the .db file
+RECREATE_DB = True  # recreates the .db file from the data files
+
+#alternate between info and debug
+logger.setLevel(logging.INFO)
+
+db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
+
+
+df = pd.read_csv('scripts/ingests/UltracoolSheet-Main.csv', usecols=['name','plx_UKIRT', 'plxerr_UKIRT','ref_plx_UKIRT']).dropna()
+df.reset_index(inplace=True, drop=True)
+print(df)
+
+#Use ingest_parallax function
+ingest_parallaxes(db, df.name, df.plx_UKIRT, df.plxerr_UKIRT, df.ref_plx_UKIRT)
+
+# WRITE THE JSON FILEs
+if SAVE_DB:
+    db.save_database(directory='data/')
