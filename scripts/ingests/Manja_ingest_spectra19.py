4acf82f47adcb09f43bacb43d8272fd5cf2a17c0
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2021-12-15 18:50:17 -0500

    Added the 77 spectra to the database, fixed the missing reference for Luhman 16 and seperately ingested the Luhman16A and Luhman16B.

--- scripts/ingests/Manja_ingest_spectra19.py
+++ scripts/ingests/Manja_ingest_spectra19.py
@@ -9,11 +9,10 @@ logger.setLevel(logging.INFO)
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 # Read in CSV file as Astropy table
-# file = 'Manja16_spectra7.csv'
-file = 'Manja19_spectra - Sheet1.csv'
-data = Table.read('scripts/ingests/' + file)
+data = Table.read('scripts/ingests/Manja19_spectra9.csv')
 
 # Ingest new instrumentation and telescope and mode
+
 ingest_instrument(db, telescope='HST', instrument='WFC3', mode='G141 grism')
 
 # Ingest new publications
@@ -29,12 +28,17 @@ ingest_publication(db, doi='10.1086/312458')
 ingest_publication(db, doi='10.1086/586735')
 
 ingest_sources(db, 'NAME Luhman 16A', 'Luhm13', search_db=False)
-# ingest_sources(db, data['Source'], data['discovery reference'])
+ingest_sources(db, 'NAME Luhman 16B', 'Luhm13', search_db=False)
+
+ingest_sources(db, data['Source'], data['discovery reference'])
+
+# Fixing missing reference for Luhman 16
+db.Sources.update().where(db.Sources.c.source == 'Luhman 16').values(reference='Luhm13').execute()
 
 # Add the spectra to the database
-# ingest_spectra(db, data['Source'], data['Spectrum'], 'em.IR.NIR', 'HST', 'WFC3', 'G141 grism',
-#               data['observation_date'],
-#               'Manj19', 'micron', 'erg/cm2/s/A', comments=data['spectrum comments'])
+ingest_spectra(db, data['Source'], data['Spectrum'], 'em.IR.NIR', 'HST', 'WFC3', 'G141 grism',
+               data['observation_date'],
+               'Manj19', 'micron', 'erg/cm2/s/A', comments=data['spectrum comments'])
 
 # WRITE THE JSON FILES
 if SAVE_DB:
