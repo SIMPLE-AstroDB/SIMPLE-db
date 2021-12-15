fa783634bcae5b9725d274302c98e0b261dc33ac
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2021-12-15 01:32:34 -0500

    Ingested publications for spectra that did not have reference in the publication table, modified the csv to include those changes.

--- scripts/ingests/Manja_ingest_spectra19.py
+++ scripts/ingests/Manja_ingest_spectra19.py
@@ -1,7 +1,7 @@
 from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
 
-SAVE_DB = True  # save the data files in addition to modifying the .db file
+SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
 
 logger.setLevel(logging.INFO)
@@ -9,22 +9,30 @@ logger.setLevel(logging.INFO)
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 # Read in CSV file as Astropy table
-data = Table.read('scripts/ingests/Manja16_spectra7.csv')
+data = Table.read('scripts/ingests/Manja19_spectra6.csv')
 
-ingest_instrument(db, telescope='ESO VLT', instrument='XShooter', mode='Echelle')
+# Ingest new instrumentation and telescope and mode
 
-# Add the sources to the database
-# ingest_sources(db, data['Source'])
+ingest_instrument(db, telescope='HST', instrument='WFC3', mode='G141 grism')
 
-simp = db.Sources.update()\
-         .where(db.Sources.c.source == 'SIMP J013656.5+093347.3')\
-         .values(reference='Arti06')
-db.engine.execute(simp)
+ingest_publication(db, doi='10.1038/nature08245')
+ingest_publication(db, doi='10.1093/pasj/62.6.L61')
+ingest_publication(db, doi='10.1111/j.1365-2966.2009.14763.x')
+ingest_publication(db, doi='10.1093/mnras/stw1969')
+ingest_publication(db, doi='10.1051/0004-6361/201117081')
+ingest_publication(db, doi='10.1093/mnras/stu1715')
+ingest_publication(db, doi='10.1086/519793')
+ingest_publication(db, bibcode='2011AAS...21710304R ')
+ingest_publication(db, doi='10.1086/312458')
+ingest_publication(db, doi='10.1086/586735')
+
+
+ingest_sources(db, data['Source'], data['discovery reference'])
 
 # Add the spectra to the database
-ingest_spectra(db, data['Source'], data['Spectrum'], data['regime'], 'ESO VLT', 'XShooter', 'Echelle',
+ingest_spectra(db, data['Source'], data['Spectrum'], 'em.IR.NIR', 'HST', 'WFC3', 'G141 grism',
                data['observation_date'],
-               'Manj16', comments=data['spectrum comments'])
+               'Manj19', 'micron', 'erg/cm2/s/A', comments=data['spectrum comments'])
 
 # WRITE THE JSON FILES
 if SAVE_DB:
