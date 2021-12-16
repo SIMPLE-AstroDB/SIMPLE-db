4ba1fbd20b9b160b17460b56351fa2d12fa7f09c
kelle <kellecruz@gmail.com>
2021-12-16 14:08:34 -0500

    alt way to get rid of empty rows added in comment

--- scripts/ingests/2020Best_parallax_UKIRT.py
+++ scripts/ingests/2020Best_parallax_UKIRT.py
@@ -16,6 +16,11 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 df = pd.read_csv('scripts/ingests/UltracoolSheet-Main.csv', usecols=['name','plx_UKIRT', 'plxerr_UKIRT','ref_plx_UKIRT']).dropna()
 df.reset_index(inplace=True, drop=True)
 print(df)
+# if we wanted to use an astropy tables method instead,
+# we could use this to create a new table without masked plx_UKIRT values
+# see https://community.openastronomy.org/t/extract-unmasked-elements-from-astropy-table/145/6?u=kelle
+# data = Table.read('scripts/ingests/UltracoolSheet-Main.csv')
+# UKIRT_plx_data = data[~data['plx_UKIRT'].mask]
 
 #Use ingest_parallax function
 ingest_parallaxes(db, df.name, df.plx_UKIRT, df.plxerr_UKIRT, df.ref_plx_UKIRT)
