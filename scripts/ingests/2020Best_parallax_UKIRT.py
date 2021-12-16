d73d7587f68b8a551bffaa81d7ad3cc0110f87e5
kelle <kellecruz@gmail.com>
2021-12-16 14:55:45 -0500

    script polish

--- scripts/ingests/2020Best_parallax_UKIRT.py
+++ scripts/ingests/2020Best_parallax_UKIRT.py
@@ -1,5 +1,3 @@
-import logging
-
 import pandas as pd
 from scripts.ingests.ingest_utils import ingest_parallaxes
 from scripts.ingests.utils import *
@@ -12,10 +10,11 @@ logger.setLevel(logging.INFO)
 
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
-
+# Use Pandas to read in non-null rows.
 df = pd.read_csv('scripts/ingests/UltracoolSheet-Main.csv', usecols=['name','plx_UKIRT', 'plxerr_UKIRT','ref_plx_UKIRT']).dropna()
 df.reset_index(inplace=True, drop=True)
 print(df)
+
 # if we wanted to use an astropy tables method instead,
 # we could use this to create a new table without masked plx_UKIRT values
 # see https://community.openastronomy.org/t/extract-unmasked-elements-from-astropy-table/145/6?u=kelle
