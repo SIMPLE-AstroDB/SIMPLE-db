a1e9d845c460964c9ba5d41342111bf0ac1a8bdb
David Rodriguez <drodriguez@stsci.edu>
2022-01-14 14:19:03 -0500

    script to update spectra units to make them astropy-resolvable

---
+++ scripts/updates/update_spectra_units.py
@@ -0,0 +1,21 @@
+# File to fix spectra units in database
+from scripts.ingests.utils import load_simpledb
+from sqlalchemy import func
+
+# Establish connection to database
+db = load_simpledb('SIMPLE.db', recreatedb=True)
+
+# Get list of units
+t = db.query(db.Spectra.c.flux_units, func.count(db.Spectra.c.flux_units).label('Counts')).\
+    group_by(db.Spectra.c.flux_units).\
+    all()
+for row in t:
+    print(row)
+
+# Fix units
+db.Spectra.update().where(db.Spectra.c.flux_units == 'Wm-2um-1').values(flux_units='W m-2 um-1').execute()
+db.Spectra.update().where(db.Spectra.c.flux_units == 'ergs-1cm-2A-1').values(flux_units='erg s-1 cm-2 A-1').execute()
+db.Spectra.update().where(db.Spectra.c.flux_units == 'ergs s-1 cm-2 A-1').values(flux_units='erg s-1 cm-2 A-1').execute()
+
+# Save database
+db.save_database('data/')
