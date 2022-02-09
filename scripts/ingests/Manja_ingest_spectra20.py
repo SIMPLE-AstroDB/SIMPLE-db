324f9ca23c4738f5338d901f8f8269c64ddccc74
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2022-02-08 21:01:46 -0500

    Added the discovery reference for the spectra

--- scripts/ingests/Manja_ingest_spectra20.py
+++ scripts/ingests/Manja_ingest_spectra20.py
@@ -1,10 +1,12 @@
+import logging
+
 from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
 
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
@@ -13,13 +15,15 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 file = 'Manja20_spectra - Sheet1.csv'
 data = Table.read('scripts/ingests/' + file)
 
+ingest_publication(db, bibcode='2010A&A...510A..27B ')
 ingest_sources(db, data['Source'], data['discovery reference'])
 
+
 # Add the spectra to the database
-ingest_spectra(db, data['Source'], data['Spectrum'], 'em.IR.NIR', 'ESO VLT', 'XShooter', 'Echelle',
-               data['observation_date'],
-               'Manj20', 'nm', 'erg/cm^2^/s/micron', comments=data['spectrum comments'],
-               other_references=data['Other spectrum refs'])
+# ingest_spectra(db, data['Source'], data['Spectrum'], 'em.IR.NIR', 'ESO VLT', 'XShooter', 'Echelle',
+#                data['observation_date'],
+#                'Manj20', 'nm', 'erg/cm^2^/s/micron', comments=data['spectrum comments'],
+#                other_references=data['Other spectrum refs'])
 
 # WRITE THE JSON FILES
 if SAVE_DB:
