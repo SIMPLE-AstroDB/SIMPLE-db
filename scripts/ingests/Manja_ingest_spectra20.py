1ac235a491ed1460a8cea450ad1c5dde498d028c
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2022-02-09 16:59:24 -0500

    SIMBAD mirror site url change

--- scripts/ingests/Manja_ingest_spectra20.py
+++ scripts/ingests/Manja_ingest_spectra20.py
@@ -5,6 +5,7 @@ from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
 # conf.server = 'simbad.harvard.edu'
 # Simbad.SIMBAD_URL='http://harvard.simbad.edu/simbad/sim-script'
+
 Simbad.URL = 'harvard.simbad.edu'
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
