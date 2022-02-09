d3dc5ddcb52306bcec81e6f3d1f144d881b56a15
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2022-02-09 14:59:54 -0500

    New changes

--- scripts/ingests/Manja_ingest_spectra20.py
+++ scripts/ingests/Manja_ingest_spectra20.py
@@ -1,7 +1,9 @@
 import logging
 
+# from astroquery.simbad import conf
 from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
+# conf.server = 'simbad.harvard.edu'
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
