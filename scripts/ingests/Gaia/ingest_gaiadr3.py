f31ada2e284e4180c0bc6cd984a0be5f105e24ee
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2022-06-13 16:58:31 -0400

    new changes

--- scripts/ingests/Gaia/ingest_gaiadr3.py
+++ scripts/ingests/Gaia/ingest_gaiadr3.py
@@ -17,16 +17,21 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 logger.setLevel(logging.DEBUG)
 
-
 # input_table = 'radial_velocity'
 
 # Functions
+# USE DR2 DESIGNATIONS TO GET DR3 DESIGNATIONS
+# gaiadr3_names = query_gaia_dr3(dr2_desig_file_string)
+dr3_desig_file_string = 'scripts/ingests/Gaia/gaia_dr3_designations_' + DATE_SUFFIX + '.xml'
+# gaiadr3_names.write(dr3_desig_file_string, format='votable', overwrite=True)
+gaiadr3_names = Table.read(dr3_desig_file_string, format='votable')
 
-def query_gaiadr3(input_table):
+
+def query_gaia_dr3(input_table):
     print('Gaia DR3 query started')
-    gaia_query_string = "SELECT *,upload_table.db_names FROM gaiadr3.gaia_source " \
+    gaia_query_string = "SELECT *,upload_table.db_names FROM gaiadr3.gaia_source" \
                         "INNER JOIN tap_upload.upload_table ON " \
-                        "gaiadr3.gaia_source.source_id = tap_upload.upload_table.dr3_source_id  "
+                        "gaiadr3.gaia_source.designation = tap_upload.upload_table.designation"
     job_gaia_query = Gaia.launch_job(gaia_query_string, upload_resource=input_table,
                                      upload_table_name="upload_table", verbose=VERBOSE)
 
@@ -38,26 +43,20 @@ def query_gaiadr3(input_table):
 
 
 def update_ref_tables():
-    ingest_publication(db, doi='10.1051/0004-6361/202243940', publication='GaiaDR3')
-    db.save_reference_table('Publications', 'data')
+    ingest_publication(db, doi='10.1051/0004-6361/202243940', publication='GaiaDR3',
+                       description='Gaia Data Release 3.Summary of the content and survey properties', ignore_ads=True)
 
 
 update_ref_tables()
 
+#
+# def add_gaia_rvs(gaia_data, ref):
+#     unmasked_rvs = np.logical_not(gaia_data['radial_velocity'].mask).nonzero
+#     rvs = gaia_data[unmasked_rvs]['gaiadr3_names', 'radial_velocity', 'radial_velocity_error']
+#     refs = [ref] * len(rvs)
+#     ingest_radial_velocities(db, rvs['db_names'], rvs['radial_velocity'], rvs['radial_velocity_error'], refs,
+#                              verbose=VERBOSE)
+#     return
 
-def add_gaia_rvs(gaia_data, ref):
-    unmasked_rvs = np.logical_not(gaia_data['radial_velocity'].mask).nonzero
-    rvs = gaia_data[unmasked_rvs]['db_names', 'radial_velocity', 'radial_velocity_error']
-    refs = [ref] * len(rvs)
-    ingest_radial_velocities(db, rvs['db_names'], rvs['radial_velocity'], rvs['radial_velocity_error'], refs,
-                             verbose=VERBOSE)
-    return
 
-
-# USE DR2 DESIGNATIONS TO GET DR3 DESIGNATIONS
-# gaiadr3_names = query_gaiadr3_names_from_dr2(dr2_desig_file_string)
-dr3_desig_file_string = 'scripts/ingests/Gaia/gaia_dr3_designations_' + DATE_SUFFIX + '.xml'
-# gaiadr3_names.write(dr3_desig_file_string, format='votable', overwrite=True)
-gaiadr3_names = Table.read(dr3_desig_file_string, format='votable')
-
-add_gaia_rvs(gaiadr3_names, 'GaiaDR3')
+# add_gaia_rvs(gaiadr3_names, 'GaiaDR3')
