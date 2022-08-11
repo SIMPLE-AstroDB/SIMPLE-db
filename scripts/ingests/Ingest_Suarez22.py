e569a93141a7feb28bc6af086912de8c13f33e22
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2022-08-11 16:31:49 -0400

    Added some comments

--- scripts/ingests/Ingest_Suarez22.py
+++ scripts/ingests/Ingest_Suarez22.py
@@ -9,13 +9,18 @@ logger.setLevel(logging.DEBUG)
 
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
+# Reading in File
 file = 'IRS Spectrum - Final Vers  - Sheet1.csv'
 data = Table.read('scripts/ingests/' + file)
 
+# Ingesting missing publications
 ingest_publication(db, bibcode='2022MNRAS.513.5701S', publication='Suar22')
 ingest_publication(db, bibcode='1952ApJ...115..582L')
+
+# Ingesting the sources along with the necessary discovery reference
 ingest_sources(db, data['Source'], data['discovery reference'])
 
+# Used new parameter for ingest spectra to add original spectrum
 ingest_spectra(db, data['Source'], data['spectrum'], 'mir', 'Spitzer', 'IRS', 'SL',
                data['observation_date'],
                'Suar22', original_spectra=data['original_spectrum'], wavelength_units='um', flux_units='Jy', comments=data['spectrum comments'])
