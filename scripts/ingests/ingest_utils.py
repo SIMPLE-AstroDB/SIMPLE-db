377f7d48d1392919791039ff9804572b22db0324
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2022-08-11 16:03:52 -0400

    Changed ingest_spectra function to better fit the current format of the spectra table

--- scripts/ingests/ingest_utils.py
+++ scripts/ingests/ingest_utils.py
@@ -868,7 +868,7 @@ def ingest_photometry(db, sources, bands, magnitudes, magnitude_errors, referenc
 
 
 # SPECTRA
-def ingest_spectra(db, sources, spectra, regimes, telescopes, instruments, modes, obs_dates, references,
+def ingest_spectra(db, sources, spectra, regimes, telescopes, instruments, modes, obs_dates, references,original_spectra=None,
                    wavelength_units=None, flux_units=None, wavelength_order=None,
                    comments=None, other_references=None, raise_error=True):
     """
@@ -892,6 +892,8 @@ def ingest_spectra(db, sources, spectra, regimes, telescopes, instruments, modes
         List of strings or datetime objects
     references: list[str]
         List or string
+    original_spectra: list[str]
+        List of filenames corresponding to original spectra files
     wavelength_units: list[str] or Quantity, optional
         List or string
     flux_units: list[str] or Quantity, optional
@@ -944,7 +946,9 @@ def ingest_spectra(db, sources, spectra, regimes, telescopes, instruments, modes
         internet = check_internet_connection()
         if internet:
             request_response = requests.head(spectra[i])
+            request_response1 = requests.head(original_spectra[i])
             status_code = request_response.status_code  # The website is up if the status code is 200
+            status_code1 = request_response1.status_code
             if status_code != 200:
                 n_skipped += 1
                 msg = "The spectrum location does not appear to be valid: \n" \
@@ -957,6 +961,19 @@ def ingest_spectra(db, sources, spectra, regimes, telescopes, instruments, modes
                     continue
             else:
                 msg = f"The spectrum location appears up: {spectra[i]}"
+                logger.debug(msg)
+            if status_code1 != 200:
+                n_skipped += 1
+                msg = "The spectrum location does not appear to be valid: \n" \
+                      f'spectrum: {original_spectra[i]} \n' \
+                      f'status code: {status_code1}'
+                logger.error(msg)
+                if raise_error:
+                    raise SimpleError(msg)
+                else:
+                    continue
+            else:
+                msg = f"The spectrum location appears up: {original_spectra[i]}"
                 logger.debug(msg)
         else:
             msg = "No internet connection. Internet is needed to check spectrum files."
@@ -992,6 +1009,7 @@ def ingest_spectra(db, sources, spectra, regimes, telescopes, instruments, modes
         # TODO: make it possible to ingest units and order
         row_data = [{'source': db_name,
                      'spectrum': spectra[i],
+                     'original_spectrum':None if ma.is_masked(original_spectra[i]) else original_spectra[i],
                      'local_spectrum': None,  # if ma.is_masked(local_spectra[i]) else local_spectra[i],
                      'regime': regimes[i],
                      'telescope': telescopes[i],
