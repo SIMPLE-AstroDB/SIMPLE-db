cac15500cd480ef7e42b3f035d695781d4dca83b
kelle <kellecruz@gmail.com>
2022-10-06 10:19:05 -0700

    add ingest_gaia_pms function

--- scripts/ingests/ingest_utils.py
+++ scripts/ingests/ingest_utils.py
@@ -1235,13 +1235,31 @@ def ingest_instrument(db, telescope=None, instrument=None, mode=None):
 
     return
 
-def ingest_gaia_photometry(db, gaia_data, ref):
+
+def get_gaiadr3(gaia_id, verbose=True):
+    # Currently setup just to query one source
+    # TODO: add some debug and info messages
+    gaia_query_string = f"SELECT " \
+                        f"parallax, parallax_error, " \
+                        f"pmra, pmra_error, pmdec, pmdec_error " \
+                        f"phot_g_mean_flux, phot_g_mean_flux_error, phot_g_mean_mag," \
+                        f"phot_rp_mean_flux, phot_rp_mean_flux_error, phot_rp_mean_mag" \
+                        f"FROM gaiadr3.gaia_source WHERE " \
+                        f"gaiadr3.gaia_source.source_id = '{gaia_id}'"
+    job_gaia_query = Gaia.launch_job(gaia_query_string, verbose=verbose)
+
+    gaia_data = job_gaia_query.get_results()
+
+    return gaia_data
+
+
+def ingest_gaia_photometry(db, sources, gaia_data, ref):
     unmasked_gphot = np.logical_not(gaia_data['phot_g_mean_mag'].mask).nonzero()
-    gaia_g_phot = gaia_data[unmasked_gphot]['db_names', 'phot_g_mean_flux', 'phot_g_mean_flux_error',
+    gaia_g_phot = gaia_data[unmasked_gphot]['phot_g_mean_flux', 'phot_g_mean_flux_error',
                                             'phot_g_mean_mag']
 
     unmased_rpphot = np.logical_not(gaia_data['phot_rp_mean_mag'].mask).nonzero()
-    gaia_rp_phot = gaia_data[unmased_rpphot]['db_names', 'phot_rp_mean_flux', 'phot_rp_mean_flux_error',
+    gaia_rp_phot = gaia_data[unmased_rpphot]['phot_rp_mean_flux', 'phot_rp_mean_flux_error',
                                              'phot_rp_mean_mag']
 
     # e_Gmag=abs(-2.5/ln(10)*e_FG/FG) from Vizier Note 37 on Gaia DR2 (I/345/gaia2)
@@ -1259,10 +1277,10 @@ def ingest_gaia_photometry(db, gaia_data, ref):
     else:
         raise Exception
 
-    ingest_photometry(db, gaia_g_phot['db_names'], g_band_name, gaia_g_phot['phot_g_mean_mag'], gaia_g_phot['g_unc'],
+    ingest_photometry(db, sources, g_band_name, gaia_g_phot['phot_g_mean_mag'], gaia_g_phot['g_unc'],
                       ref, ucds='em.opt', telescope='Gaia', instrument='Gaia')
 
-    ingest_photometry(db, gaia_rp_phot['db_names'], rp_band_name, gaia_rp_phot['phot_rp_mean_mag'],
+    ingest_photometry(db, sources, rp_band_name, gaia_rp_phot['phot_rp_mean_mag'],
                       gaia_rp_phot['rp_unc'], ref, ucds='em.opt.R', telescope='Gaia', instrument='Gaia')
 
     return
@@ -1271,17 +1289,18 @@ def ingest_gaia_photometry(db, gaia_data, ref):
 def ingest_gaia_parallaxes(db, sources, gaia_data, ref):
     unmasked_pi = np.logical_not(gaia_data['parallax'].mask).nonzero()
     gaia_parallaxes = gaia_data[unmasked_pi]['parallax', 'parallax_error']
-    print(gaia_parallaxes)
+
     ingest_parallaxes(db, sources, gaia_parallaxes['parallax'],
                       gaia_parallaxes['parallax_error'], ref)
 
 
-def get_gaiadr3(gaia_id, verbose=True):
-    gaia_query_string = f"SELECT parallax, parallax_error FROM gaiadr3.gaia_source WHERE " \
-                        f"gaiadr3.gaia_source.source_id = '{gaia_id}'"
-    job_gaia_query = Gaia.launch_job(gaia_query_string, verbose=verbose)
+def ingest_gaia_pms(db, sources, gaia_data, ref):
+    unmasked_pms = np.logical_not(gaia_data['pmra'].mask).nonzero()
+    pms = gaia_data[unmasked_pms]['pmra', 'pmra_error', 'pmdec', 'pmdec_error']
+    refs = [ref] * len(pms)
 
-    gaia_data = job_gaia_query.get_results()
-    print(gaia_data)
+    ingest_proper_motions(db, sources,
+                          pms['pmra'], pms['pmra_error'],
+                          pms['pmdec'], pms['pmdec_error'],
+                          refs)
 
-    return gaia_data
