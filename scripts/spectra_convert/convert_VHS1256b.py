d8298f055e7fc187a622f0aca0a56774587d0c64
kelle <kellecruz@gmail.com>
2022-10-11 18:15:43 -0400

    added two more spectra.

--- scripts/spectra_convert/convert_VHS1256b.py
+++ scripts/spectra_convert/convert_VHS1256b.py
@@ -1,29 +1,32 @@
 from convert_spectra_to_spec1dfits import *
 from astropy.table import Table
 import astropy.units as u
+import logging
+
+
+logger.setLevel(logging.DEBUG)
 
 
 # Function for reading in the VHS 1256b files from Miles 2018
-def spectrum_loader(spectrum_path):
+def nirspec_spectrum_loader(spectrum_path):
     print(spectrum_path)
     spectrum_table = Table.read(spectrum_path, format='ascii',
                                 names=['wavelength', 'flux', 'flux_uncertainty'],
                                 units=[u.micron, u.watt/u.cm/u.cm/u.micron, u.watt/u.cm/u.cm/u.micron ])
     return spectrum_table
 
-# make an Astropy table instead of a CSV file
 
-# outside of loop. Same for every spectrum.
+def osiris_spectrum_loader(spectrum_path):
+    print(spectrum_path)
+    spectrum_table = Table.read(spectrum_path, format='ascii',
+                                names=['wavelength', 'flux', 'flux_uncertainty'],
+                                units=[u.angstrom, u.watt/u.cm/u.cm/u.angstrom, u.watt/u.cm/u.cm/u.angstrom ])
+    return spectrum_table
+
+
+# Same for every spectrum.
 dataset_info = {
-    'loader_function': spectrum_loader,  # Function which loads the spectrum into an Astropy Table
     'fits_data_dir': '/Users/kelle/Dropbox (Personal)/Mac (3)/Downloads/VHS1256b/',  # Path of FITS output
-
-    # Information about the publications the data come from
-    'title': 'Methane in Analogs of Young Directly Imaged Exoplanets',  # Title of Paper
-    'author': 'Miles et al.',  # Authors of paper
-    'bibcode': '2018ApJ...869...18M',  # Bibcode of paper
-    'doi': '10.3847/1538-4357/aae6cd',  # DOI of paper
-    # 'dataset_comments': None,  # Any comments about the data provenance
 
     # Information about new spectra
     'vopub': 'SIMPLE Archive',  # SIMPLE Archive, if going to be served via SIMPLE
@@ -36,12 +39,11 @@ dataset_info = {
     'dec': -12.957692,  # float, decimal degrees
 }
 
-# Start loop over spectra here if more than one.
-
-# First spectrum - NIRSPEC
+# NIRSPEC spectrum
 NIRSPEC_spectrum_info = {
+    'loader_function': nirspec_spectrum_loader,  # Function which loads the spectrum into an Astropy Table
     'bandpass': 'nir',
-    'aperture': '0.380',  # [arcseconds]
+    'aperture': '0.570',  # [arcseconds]
     "observatory": 'Keck',  # From https://github.com/astropy/astropy-data/blob/gh-pages/coordinates/sites.json
     'telescope': 'Keck II',
     'instrument': 'NIRSPEC',
@@ -50,53 +52,79 @@ NIRSPEC_spectrum_info = {
     # "stop_time":
     # "exposure_time": , # in seconds
     'observation_date': '2016-06-19' ,  # YYYY-MM-DD
-    # 'spectrum_comments':
+    'spectrum_comments': 'KL filter, 2.9-4.4 microns with R of 1300',
+    # Information about the publications the data come from
+    'title': 'Methane in Analogs of Young Directly Imaged Exoplanets',  # Title of Paper
+    'author': 'Miles et al.',  # Authors of paper
+    'bibcode': '2018ApJ...869...18M',  # Bibcode of paper
+    'doi': '10.3847/1538-4357/aae6cd',  # DOI of paper
+    # 'dataset_comments': None,  # Any comments about the data provenance
 }
 
-spectrum_info_all = {**dataset_info, **NIRSPEC_spectrum_info}
+nirspec_spectrum_info_all = {**dataset_info, **NIRSPEC_spectrum_info}
+# convert_to_fits(nirspec_spectrum_info_all)
 
-# convert_to_fits(spectrum_info_all)
-
-# Second spectrum - Optical
-optical_spectrum_info = {
-    'bandpass': 'nir',
-    'aperture': '0.380',  # [arcseconds]
-    'observatory': 'Keck',  # From https://github.com/astropy/astropy-data/blob/gh-pages/coordinates/sites.json
-    'telescope': 'Keck II',
-    'instrument': 'NIRSPEC',
-    'file_path': "/Users/kelle/Dropbox (Personal)/Mac (3)/Downloads/vhs1256b_spectra_Figure8_Miles2018.txt",
+# Optical OSIRIS spectrum
+osiris_spectrum_info = {
+    'loader_function': osiris_spectrum_loader,  # Function which loads the spectrum into an Astropy Table
+    'bandpass': 'opt',
+    'aperture': '1.5',  # [arcseconds]
+    'observatory': 'lapalma',  # From https://github.com/astropy/astropy-data/blob/gh-pages/coordinates/sites.json
+    'telescope': 'GTC',
+    'instrument': 'OSIRIS',
+    'file_path': "/Users/kelle/Dropbox (Personal)/Mac (3)/Downloads/vhs1256b/vhs1256b_opt_Osiris.dat",
     # "start_time":  , # [MJD]
     # "stop_time": # [MJD]
     # "exposure_time": , # [seconds]
-    'observation_date': '2016-06-19' ,  # YYYY-MM-DD
-    # 'spectrum_comments':
+    'observation_date': '2014-06-03' ,  # YYYY-MM-DD
+    'spectrum_comments': 'R300 grating, R~130'
 }
-spectrum_info_all = {**dataset_info, **optical_spectrum_info}
-# convert_to_fits(spectrum_info_all)
+osiris_spectrum_info_all = {**dataset_info, **osiris_spectrum_info}
+convert_to_fits(osiris_spectrum_info_all)
 
-# END LOOP
+# Optical OSIRIS spectrum
+sofi_spectrum_info = {
+    'loader_function': osiris_spectrum_loader,  # Function which loads the spectrum into an Astropy Table
+    'bandpass': 'nir',
+    'aperture': '1.0',  # [arcseconds]
+    'observatory': 'lasilla',  # From https://github.com/astropy/astropy-data/blob/gh-pages/coordinates/sites.json
+    'telescope': 'NTT',
+    'instrument': 'SofI',
+    'file_path': "/Users/kelle/Dropbox (Personal)/Mac (3)/Downloads/vhs1256b/vhs1256b_nir_SOFI.dat",
+    # "start_time":  , # [MJD]
+    # "stop_time": # [MJD]
+    # "exposure_time": , # [seconds]
+    'observation_date': '2014-03-12' ,  # YYYY-MM-DD
+    'spectrum_comments': 'blue and red grisms, covering 950-2520 nm'
+}
+sofi_spectrum_info_all = {**dataset_info, **sofi_spectrum_info}
+convert_to_fits(sofi_spectrum_info_all)
+
 
 # Plot the newly converted files using specutils
-from glob import glob
 from astropy.io import fits
 from matplotlib import pyplot as plt
 from specutils import Spectrum1D
 
-files = glob(spectrum_info_all['fits_data_dir'] + '*.fits')
+files = [
+    f"{osiris_spectrum_info_all['fits_data_dir']}vhs1256b_spectra_Figure8_Miles2018.fits",
+    f"{osiris_spectrum_info_all['fits_data_dir']}vhs1256b_opt_Osiris.fits",
+    f"{sofi_spectrum_info_all['fits_data_dir']}vhs1256b_nir_SOFI.fits",
+
+    ]
 
 for fits_file in files:
+    print(fits_file)
     spec1d = Spectrum1D.read(fits_file, format='tabular-fits')
-    fits = fits.open(fits_file)
-    header = fits[0].header
-    name = header['OBJECT']
+    name = spec1d.meta['SPECTRUM']
 
 
     ax = plt.subplots()[1]
     # ax.plot(spec1d.spectral_axis, spec1d.flux)
-    ax.errorbar(spec1d.spectral_axis.value, spec1d.flux.value, yerr=spec1d.uncertainty.array, fmt='o')
+    ax.errorbar(spec1d.spectral_axis.value, spec1d.flux.value, yerr=spec1d.uncertainty.array, fmt='-')
     ax.set_xlabel(f"Dispersion ({spec1d.spectral_axis.unit})")
-    ax.set_ylabel(f"Flux ({spec1d.flux.unit})")  #try to get from header
-    plt.xlim([2.8,4.0])
-    plt.ylim([0,max(spec1d.flux[0:400]).value*1.50])
+    ax.set_ylabel(f"Flux ({spec1d.flux.unit})")
+    # plt.xlim([min(spec1d.spectral_axis.value),max(spec1d.spectral_axis.value)])
+    # plt.ylim([0,max(spec1d.flux[0:400]).value*1.50])
     plt.title(name)
     plt.show()
