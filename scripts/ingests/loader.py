3487ee921f8de5d1986cc7f371561586048c8f8e
Jolie L'Heureux <jel2227@columbia.edu>
2022-11-10 15:33:49 -0500

    loader finds wavelenght in txt file

--- scripts/ingests/loader.py
+++ scripts/ingests/loader.py
@@ -1,17 +1,3 @@
-from urllib.request import urlopen
-from astropy.table import Table
-import astropy.units as u
-
-#reorganize loader
-#find start of header
-#end of header
-#start of data
-#use data_start in Table
-#loop thorugh the file until you find wavelength
-#data starts at |wavelength
-#try to no use names=[]
-# adn untis = [] bc shoudl be able to auto identify
-#fomrat is: format = 'ipac'
 
 #function for loading the data##
 #############################################
@@ -23,35 +9,22 @@ import astropy.units as u
 #add date file was generate and converted by reprocessed
 #pass dictionary to convert_to_fits()   function!!!
 
-#
+from urllib.request import urlopen
+from astropy.table import Table
 
 spectrum_path = 'https://bdnyc.s3.amazonaws.com/BRI+0021-0214.txt'
-wavelength = []
-flux = []
-flux_unc = []
 
 def text_spectrum_loader(spectrum_path):
-    data = urlopen(spectrum_path)
-    lines = data.readlines()[243:]
-    # find start of data using string matching
-    header_end = 240
-    header = lines[:header_end]
-    data_start = 243
-    spectrum_table = Table([wavelength,flux,flux_unc],
-                                names=['wavelength', 'flux', 'flux_uncertainty'], #may not need
-                                units=[u.micron, u.Jy, u.Jy],
-                           data_start = data_start)
-"""    for line in lines: # files are iterable
-        split_line = line.split()
-
-        wavelength.append(split_line[0])
-        flux.append(split_line[1])
-        flux_unc.append(split_line[2])
-        spectrum_table = Table([wavelength,flux,flux_unc],
-                                names=['wavelength', 'flux', 'flux_uncertainty'],
-                                units=[u.micron, u.Jy, u.Jy])"""
+    data = urlopen(spectrum_path).read().decode('UTF-8')
+    header_end = '|wavelength'
+    lines = data.splitlines()
+    for i in range(len(lines)):
+        if header_end in lines[i]:
+            data_start = i
+    header = lines[:data_start]
+    spectrum_table = Table.read(data, format='ipac',
+                                    data_start = data_start)#need to convert units to Jy
     print(spectrum_table)
-
     return spectrum_table
 
 text_spectrum_loader(spectrum_path)
@@ -59,30 +32,3 @@ text_spectrum_loader(spectrum_path)
 
 #function for loading the data
 #nned to generate dictionaries -> should just need one dictionary
-
-
-
-
-
-
-'''def text_spectrum_loader(spectrum_path):
-    data = urlopen(spectrum_path) # it's a file like object and works just like a file
-    for line in data: # files are iterable
-        split_line = line.split()
-        if split_line:
-            if split_line[0] == '\\\\':
-                line[0] = line[0]+line[1]
-                line.delete[1]
-            #add the zeroeth element as the key to your dictionary and the rest as a value of that key in your dict
-            #dict.add(split_line[0], split_line[1:])
-        #How to extract header data?
-        try:
-            print(split_line[0])
-            print(split_line[1])
-        except IndexError:
-            pass
-
-    return
-
-text_spectrum_loader(spectrum_path)'''
-
--- scripts/spectra_convert/autoreader.py
+++ scripts/spectra_convert/autoreader.py
@@ -4,9 +4,8 @@ from astropy.io import ascii
 
 table= 'wcs1d_spectrum_table.dat'
 data = ascii.read(table)
-spectrum = data['wcs1d-fits Spectra']
 
 for row in data:
     specs = urllib.request.urlopen(row['wcs1d-fits Spectra'])
-    Spectrum1D.read(specs)
+    Spectrum1D.read(specs, format='wcs1d-fits')
 #does not work without format = 'wcs1d-fits' :(
