f9652cda27bc6adf424e152f7185e97cfdc5f34a
sherelynalejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2022-12-13 16:01:41 -0500

    changed to use dictionary's

--- scripts/Update_Photometry.py
+++ scripts/Update_Photometry.py
@@ -9,105 +9,28 @@ logger.setLevel(logging.INFO)
 
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
-# Knap04
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.H', db.Photometry.c.reference == 'Knap04')). \
-    values(band='UKIRT/UFTI.H').execute()
+# Add New Filters Themselves
+# Todo : Ukidss and ufti y band
 
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.J', db.Photometry.c.reference == 'Knap04')). \
-    values(band='UKIRT/UFTI.J').execute()
+# UKIRT/UFTI
+mko_ufti = {f'MKO.{band}': f'UKIRT/UFTI.{band}' for band in ('Y', 'J', 'H', 'K')}
+for ref in ('Goli04a', 'Legg02a', 'Knap04', 'Stra99'):
+    for mkoband, ukirtband in mko_ufti.items():
+        db.Photometry.update().where(and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(band=ukirtband).execute()
 
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.Y', db.Photometry.c.reference == 'Knap04')). \
-    values(band='UKIRT/UFTI.Y').execute()
+# UKIRT/UKIDSS
+mko_uki = {f'MKO.{band}': f'UKIRT/UKIDSS.{band}' for band in ('Y', 'J', 'H', 'K')}
+for ref in ('Burn10', 'Best20b', 'Legg10'):
+    for mkoband, ukirtband in mko_uki.items():
+        db.Photometry.update().where(and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(band=ukirtband).execute()
 
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.K', db.Photometry.c.reference == 'Knap04')). \
-    values(band='UKIRT/UFTI.K').execute()
-
-# Update Filter name itself
-
-db.PhotometryFilters.update().where(db.PhotometryFilters.c.band == 'MKO.H').values(band='UKIRT/UFTI.H')
-# Legg02a
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.H', db.Photometry.c.reference == 'Legg02a')). \
-    values(band='UKIRT/UFTI.H').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.J', db.Photometry.c.reference == 'Legg02a')). \
-    values(band='UKIRT/UFTI.J').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.Y', db.Photometry.c.reference == 'Legg02a')). \
-    values(band='UKIRT/UFTI.Y').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.K', db.Photometry.c.reference == 'Legg02a')). \
-    values(band='UKIRT/UFTI.K').execute()
-
-# Goli04a
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.H', db.Photometry.c.reference == 'Goli04a')). \
-    values(band='UKIRT/UFTI.H').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.J', db.Photometry.c.reference == 'Goli04a')). \
-    values(band='UKIRT/UFTI.J').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.Y', db.Photometry.c.reference == 'Goli04a')). \
-    values(band='UKIRT/UFTI.Y').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.K', db.Photometry.c.reference == 'Goli04a')). \
-    values(band='UKIRT/UFTI.K').execute()
-
-# Hewe06  on wfcam
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.H', db.Photometry.c.reference == 'Hewe06')). \
-    values(band='UKIRT/WFCAM.H').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.J', db.Photometry.c.reference == 'Hewe06')). \
-    values(band='UKIRT/WFCAM.J').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.Y', db.Photometry.c.reference == 'Hewe06')). \
-    values(band='UKIRT/WFCAM.Y').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.K', db.Photometry.c.reference == 'Hewe06')). \
-    values(band='UKIRT/WFCAM.K').execute()
-
-# Burn10 is ukidds
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.H', db.Photometry.c.reference == 'Burn10')). \
-    values(band='UKIRT/UKIDSS.H').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.J', db.Photometry.c.reference == 'Burn10')). \
-    values(band='UKIRT/UKIDSS.J').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.Y', db.Photometry.c.reference == 'Burn10')). \
-    values(band='UKIRT/UKIDSS.Y').execute()
-
-db.Photometry.update(). \
-    where(and_(db.Photometry.c.band == 'MKO.K', db.Photometry.c.reference == 'Burn10')). \
-    values(band='UKIRT/UKIDSS.K').execute()
-
-
-# Best 20a and Best 20b
-mkod = {f'MKO.{band}': f'UKIRT/WFCAM.{band}' for band in ('Y', 'J', 'H', 'K')}
-for ref in ('Best20a', 'Best20b'):
-    for mkoband, ukirtband in mkod.items():
+# UKIRT/WFCAM
+mko_wfc = {f'MKO.{band}': f'UKIRT/WFCAM.{band}' for band in ('Y', 'J', 'H', 'K')}
+for ref in ('Best20a', 'Best20b', 'Hewe06'):
+    for mkoband, ukirtband in mko_wfc.items():
         db.Photometry.update().where(and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(band=ukirtband).execute()
 
 
-# Stra99
 # Curr13a
 # Legg10
 # Legg98
