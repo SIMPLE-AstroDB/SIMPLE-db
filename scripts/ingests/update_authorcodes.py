076aef7bc125a1ec65524ef5d4fb68f87dbec743
Sherelyn Alejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2023-02-22 15:00:40 -0500

    started a sqlalclchemy query to find publications that need changing.

--- scripts/ingests/update_authorcodes.py
+++ scripts/ingests/update_authorcodes.py
@@ -1,6 +1,5 @@
 from scripts.ingests.ingest_utils import *
-import numpy.ma as ma
-from sqlalchemy import and_
+from sqlalchemy import select, or_, and_
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
@@ -10,3 +9,12 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 logger.setLevel(logging.INFO)
 
 # Start the dictionary of all the author codes that need to be updated
+
+stm = select([db.Publications.c.publication, db.Publications.c.bibcode]).where(or_
+                                                                                 (db.Publications.c.publication.like(
+                                                                                     '______a'),
+                                                                                  db.Publications.c.publication.like(
+                                                                                      '______b')))
+result = db.session.execute(stm)
+# print(result.all())
+# start at bidcode[17:-1]
