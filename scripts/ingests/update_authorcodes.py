3e6d1e2b954823c492e8c01149c51a732c76f65f
Sherelyn Alejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2023-02-23 16:02:42 -0500

    changed the result to a pandas dataframe

--- scripts/ingests/update_authorcodes.py
+++ scripts/ingests/update_authorcodes.py
@@ -1,5 +1,6 @@
 from scripts.ingests.ingest_utils import *
-from sqlalchemy import select, or_, and_
+from sqlalchemy import select, or_
+import pandas as pd
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
@@ -8,13 +9,13 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 logger.setLevel(logging.INFO)
 
-# Start the dictionary of all the author codes that need to be updated
-
-stm = select([db.Publications.c.publication, db.Publications.c.bibcode]).where(or_
-                                                                                 (db.Publications.c.publication.like(
-                                                                                     '______a'),
-                                                                                  db.Publications.c.publication.like(
-                                                                                      '______b')))
+#  creating a query that finds all the author codes that need to be updated
+stm = select([db.Publications.c.publication, db.Publications.c.bibcode]).where(
+    or_(db.Publications.c.publication.like('______a'), db.Publications.c.publication.like('______b'),
+        db.Publications.c.publication.like('______c'), db.Publications.c.publication.like('______d'),
+        db.Publications.c.publication.like('______e'), db.Publications.c.publication.like('______f'),
+        db.Publications.c.publication.like('______g'), db.Publications.c.publication.like('______h')))
 result = db.session.execute(stm)
-# print(result.all())
+df = pd.DataFrame(result.all())
+print(df)
 # start at bidcode[17:-1]
