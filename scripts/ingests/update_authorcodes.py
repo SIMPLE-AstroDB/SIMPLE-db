d5ab74e3822837eac16c2705a7dc363dd7027935
Sherelyn Alejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2023-02-24 16:05:41 -0500

    Generated new author codes and updated the old ones.

--- scripts/ingests/update_authorcodes.py
+++ scripts/ingests/update_authorcodes.py
@@ -1,7 +1,6 @@
 from scripts.ingests.utils import *
 from sqlalchemy import select, or_
 import pandas as pd
-import numpy as np
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
@@ -10,22 +9,46 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 logger.setLevel(logging.INFO)
 
+if RECREATE_DB:
+    db.Publications.update().where(db.Publications.c.publication == 'Luyt79a').values(
+        publication='Luyt79.5').execute()
+    db.Publications.update().where(db.Publications.c.publication == 'Luyt79b').values(
+        publication='Luyt79.2').execute()
+
 #  creating a query that finds all the author codes that need to be updated
 stm = select([db.Publications.c.publication, db.Publications.c.bibcode]).where(
     or_(db.Publications.c.publication.like('______a'), db.Publications.c.publication.like('______b'),
         db.Publications.c.publication.like('______c'), db.Publications.c.publication.like('______d'),
         db.Publications.c.publication.like('______e'), db.Publications.c.publication.like('______f'),
         db.Publications.c.publication.like('______g'), db.Publications.c.publication.like('______h')))
+
 result = db.session.execute(stm)
 df = pd.DataFrame(result.all())
-print(np.where(df[0]))
-new_publications = []
-small_names = []
+
+# Looping through old author codes and updating them
 for publication, bibcode in zip(df[0], df[1]):
     if publication != "Missing":
-        author_last_name = publication[:4]
-        new_publication_name = author_last_name + bibcode[2:4] + '.' +bibcode[17:-1]
-        new_publications.append(new_publication_name)
+        if bibcode[14:-1].find('.') == -1:
+            new_publication_name = publication[0:6] + '.' + bibcode[14:-1]
+            db.Publications.update().where(db.Publications.c.publication == publication).values(
+                publication=new_publication_name).execute()
+        elif bibcode[15:-1].find('.') == -1:
+            new_publication_name = publication[0:6] + '.' + bibcode[15:-1]
+            db.Publications.update().where(db.Publications.c.publication == publication).values(
+                publication=new_publication_name).execute()
+        elif bibcode[16:-1].find('.') == -1:
+            new_publication_name = publication[0:6] + '.' + bibcode[16:-1]
+            db.Publications.update().where(db.Publications.c.publication == publication).values(
+                publication=new_publication_name).execute()
+        elif bibcode[17:-1].find('.') == -1:
+            new_publication_name = publication[0:6] + '.' + bibcode[17:-1]
+            db.Publications.update().where(db.Publications.c.publication == publication).values(
+                publication=new_publication_name).execute()
+        elif bibcode[18:-1].find('.') == -1:
+            new_publication_name = publication[0:6] + '.' + bibcode[18:-1]
+            db.Publications.update().where(db.Publications.c.publication == publication).values(
+                publication=new_publication_name).execute()
 
-print(new_publications)
-# start at bidcode[17:-1]
+# WRITE THE JSON FILES
+if SAVE_DB:
+    db.save_database(directory='data/')
