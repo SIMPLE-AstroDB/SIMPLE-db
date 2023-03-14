4de6b2439080910dbf710e303a89ca02ba5e5cf3
Sherelyn Alejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2023-03-14 19:07:26 -0400

    Changed code to use the new SQLalchemy2 syntax

--- scripts/Update_Photometry.py
+++ scripts/Update_Photometry.py
@@ -13,41 +13,59 @@ db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 # Take out the old filters
 
 # Fix the Reid02.2806 Publication in the Photometry table
-db.Photometry.update().where(db.Photometry.c.reference == 'Reid02.2806').values(reference='Reid02.466').execute()
+if RECREATE_DB:
+    with db.engine.begin() as conn:
+        conn.execute(
+            db.Photometry.update().where(db.Photometry.c.reference == 'Reid02.2806').values(reference='Reid02.466'))
+    new_instrument = [{'name': 'NACO', 'reference': None}]
+    new_telescope = [{'name': 'ESO VLT U4', 'reference': None}]
+    new_filter = [{'band': 'NACO.Lp',
+                   'effective_wavelength': 37701.21,
+                   'instrument': 'NACO',
+                   'telescope': 'ESO VLT U4'}]
+    with db.engine.connect() as conn:
+        conn.execute(db.Instruments.insert().values(new_instrument))
+        conn.execute(db.Telescopes.insert().values(new_telescope))
+        conn.execute(db.PhotometryFilters.insert().values(new_filter))
+        conn.commit()
 
 # UKIRT/UFTI
-mko_ufti = {f'MKO.{band}': f'UKIRT/UFTI.{band}' for band in ('Y', 'J', 'H', 'K')}
-for ref in ('Goli04.3516', 'Knap04', 'Stra99', 'Geba01', 'Chiu06', 'Naud14', 'Legg00'):
-    for mkoband, uftiband in mko_ufti.items():
-        db.Photometry.update().where(and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(
-            band=uftiband).execute()
+mko_ufti = {f'MKO.{band}' for band in ("Y", "J", "H", "K", "L'")}
+mko_ufti_u = {f'UFTI.{band}' for band in ("Y", "J", "H", "K", "Lp")}
+for ref in (
+'Goli04.3516', 'Knap04', 'Stra99', 'Geba01', 'Chiu06', 'Naud14', 'Legg00', 'Legg98', 'Legg01', 'Legg02.452', 'Geba01',
+'Reid02.466', 'Goli04.3516', 'Jone96', 'Legg07.1079', 'Luhm07.570'):
+    for mkoband, uftiband in zip(mko_ufti, mko_ufti_u):
+        with db.engine.begin() as conn:
+            conn.execute(db.Photometry.update().where(
+                and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(
+                band=uftiband))
 
 # UKIRT/UKIDSS
-mko_uki = {f'MKO.{band}': f'UKIRT/UKIDSS.{band}' for band in ('Y', 'J', 'H', 'K')}
+mko_uki = {f'MKO.{band}' for band in ("Y", "J", "H", "K", "L'")}
+mko_uki_u = {f'UKIDSS.{band}' for band in ('Y', 'J', 'H', 'K', 'Lp')}
 for ref in ('Burn10.1952', 'Legg10'):
-    for mkoband, ukidband in mko_uki.items():
-        db.Photometry.update().where(and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(
-            band=ukidband).execute()
+    for mkoband, ukidband in zip(mko_uki, mko_uki_u):
+        with db.engine.begin() as conn:
+            conn.execute(db.Photometry.update().where(
+                and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(
+                band=ukidband))
 
 # UKIRT/WFCAM
-mko_wfc = {f'MKO.{band}': f'UKIRT/WFCAM.{band}' for band in ('Y', 'J', 'H', 'K')}
+mko_wfc = {f'MKO.{band}': f'WFCAM.{band}' for band in ('Y', 'J', 'H', 'K')}
 for mkoband, wfcband in mko_wfc.items():
-    db.Photometry.update().where(and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == 'Hewe06')).values(
-        band=wfcband).execute()
+    with db.engine.begin() as conn:
+        conn.execute(db.Photometry.update().where(
+            and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == 'Hewe06')).values(
+            band=wfcband))
 
 # Paranal/NACO
 for ref in ('Curr13.15', 'Chau04'):
-    db.Photometry.update().where(and_(db.Photometry.c.band == "MKO.L'", db.Photometry.c.reference == ref)).values(
-        band='Paranal/NACO.Lp').execute()
-
-# MKO/NSFCam
-mko_nsf = {f'MKO.{band}': f'MKO/NSFCam.{band}' for band in ("J", "H", "K", "L'")}
-for ref in ('Legg98', 'Legg01', 'Legg02.452','Geba01', 'Reid02.466','Goli04.3516', 'Jone96', 'Legg07.1079','Luhm07.570'):
-    for mkoband, nsfband in mko_nsf.items():
-        db.Photometry.update().where(and_(db.Photometry.c.band == mkoband, db.Photometry.c.reference == ref)).values(
-            band=nsfband).execute()
+    with db.engine.begin() as conn:
+        conn.execute(db.Photometry.update().where(
+            and_(db.Photometry.c.band == "MKO.L'", db.Photometry.c.reference == ref)).values(
+            band='NACO.Lp'))
 
 # WRITE THE JSON FILES
 if SAVE_DB:
     db.save_database(directory='data/')
-
