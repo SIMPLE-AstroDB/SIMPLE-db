9afb2dae76857c4be7cb773aee30ffc8539029d5
Sherelyn Alejandro <Sherelyn.Alejandro78@myhunter.cuny.edu>
2023-03-14 21:26:03 -0400

    Starting to Make the JSON files

--- scripts/MKO_photometry_update.py
+++ scripts/MKO_photometry_update.py
@@ -2,7 +2,7 @@ from scripts.ingests.utils import *
 from astropy.table import Table
 from sqlalchemy import and_
 
-SAVE_DB = False  # save the data files in addition to modifying the .db file
+SAVE_DB = True  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
 
 logger.setLevel(logging.INFO)
