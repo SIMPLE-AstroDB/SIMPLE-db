539d74987a8d7134018b1dbbf0dff5a527d6f726
Ally Baldelli <67712811+Ajb2307@users.noreply.github.com>
2023-03-22 14:38:32 -0400

    Starting importing table to script. Isn't reading at the moment some issue is occurring

---
+++ scripts/ingests/filip_15.py
@@ -0,0 +1,12 @@
+from astrodbkit2.astrodb import create_database
+from astrodbkit2.astrodb import Database
+from astropy.table import Table
+
+
+
+connection_string = 'sqlite:///SIMPLE.db' # connection string for a SQLite database named SIMPLE.db
+create_database(connection_string)
+db = Database(connection_string)
+db.load_database('data/')
+
+filip_table = Table.read("Fili15_table9.csv", data_start=1)
