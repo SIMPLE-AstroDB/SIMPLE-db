0176f99f09d9360a91ae4c16080bdf22e8b6ccfb
Ally Baldelli <67712811+Ajb2307@users.noreply.github.com>
2023-03-23 15:11:02 -0400

    Update scripts/ingests/filip_15.py
    Co-authored-by: Kelle Cruz <kellecruz@gmail.com>

--- scripts/ingests/filip_15.py
+++ scripts/ingests/filip_15.py
@@ -2,9 +2,10 @@ from astrodbkit2.astrodb import create_database
 from astrodbkit2.astrodb import Database
 from astropy.table import Table
 
-connection_string = 'sqlite:///SIMPLE.db'  # connection string for a SQLite database named SIMPLE.db
-create_database(connection_string)
-db = Database(connection_string)
-db.load_database('data/')
+SAVE_DB = True  # save the data files in addition to modifying the .db file
+RECREATE_DB = True  # recreates the .db file from the data files
+VERBOSE = False
+# LOAD THE DATABASE
+db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
 filip_table = Table.read("Fili15_table9.csv", data_start=1)
