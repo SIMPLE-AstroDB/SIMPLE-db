68c21b316345849632dfb65ad7f59bfb73930942
Ally Baldelli <67712811+Ajb2307@users.noreply.github.com>
2023-04-15 14:39:43 -0400

    Save_db = True

--- scripts/ingests/filip_15.py
+++ scripts/ingests/filip_15.py
@@ -6,7 +6,7 @@ from astropy.table import Table
 import astropy.units as u
 from astropy.coordinates import Angle
 
-SAVE_DB = False  # save the data files in addition to modifying the .db file
+SAVE_DB = True  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
 VERBOSE = False
 # LOAD THE DATABASE
