fc72e17d3eff69820c6c7663c1b3b0b4afecbe57
Ally Baldelli <67712811+Ajb2307@users.noreply.github.com>
2023-07-05 17:34:04 -0400

    start of file

--- scripts/ingests/companions_first_ingest.py
+++ scripts/ingests/companions_first_ingest.py
@@ -4,7 +4,7 @@ from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
 from astropy.table import Table
 import astropy.units as u
-from astropy.coordinates import Angle
+
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
