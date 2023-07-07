7b5992b31720fed8bdb7114210308cd375cc4af8
Ally Baldelli <67712811+Ajb2307@users.noreply.github.com>
2023-07-07 14:57:06 -0400

    ingest CWISE source and reference

--- scripts/ingests/companions_first_ingest.py
+++ scripts/ingests/companions_first_ingest.py
@@ -4,6 +4,7 @@ from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
 from astropy.table import Table
 import astropy.units as u
+from astropy.coordinates import Angle
 
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
@@ -11,4 +12,16 @@ RECREATE_DB = True  # recreates the .db file from the data files
 VERBOSE = False
 # LOAD THE DATABASE
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
+
+
+# Ingest CWISE J210640.16+250729.0 and its reference
+ingest_publication(db, doi = None, bibcode = None, publication = "Roth", 
+                   description = "Rothermich in prep.", ignore_ads = True)
+
+RA =   "21 06 40.1664"
+DEC = "+25 07 28.99"
+ingest_sources(db, ["CWISE J210640.16+250729.0"], references="Roth", 
+            ras= [Angle(RA, u.degree).degree], 
+            decs=[Angle(DEC, u.degree).degree], 
+            search_db=False)
 
