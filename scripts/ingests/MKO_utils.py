ace6ad5187e32c0ef952900511f22b155a2b9a80
astro-c <astro.colantoni@gmail.com>
2023-07-26 21:54:00 +0100

    Update of MKO Y filter

--- scripts/ingests/MKO_utils.py
+++ scripts/ingests/MKO_utils.py
@@ -52,10 +52,9 @@ def references(data_ref):
 
 
 def photometry_mko(db, data):
-    # ref_skip = 'Best20.42', 'Bowl15', 'Chiu06'
-    name_skip = []
     # uses reference to determine the instrument used for photometry
     ref = references(data['ref_Y_MKO'])
+    name_skip = []
     mag = data['Y_MKO']
     mag_err = data['Yerr_MKO']
 
@@ -63,10 +62,10 @@ def photometry_mko(db, data):
     wircam_name_list = ['ULAS J115038.79+094942.9']
     wfcam_ref_list = ['Burn08', 'Burn14', 'Card15', 'Deac11.6319', 'Deac12.100', 'Deac17.1126', 'Lawr07', 'Lawr12',
                       'Liu_13.20', 'Lodi07.372', 'Luca10', 'Pinf08', 'Warr07.1400']
-    wfcam_name_list = ['HD 253662B','NLTT 27966B']
-    # irac_name_list = ['2MASS J00501994-3322402', '2MASS J11145133-2618235', '2MASS J12373919+6526148']
+    wfcam_name_list = ['HD 253662B', 'NLTT 27966B']
     niri_ref_list = ['Dupu15.102', 'Lach15', 'Legg13', 'Legg15', 'Legg16', 'Liu_12']
-    vista_ref_list = ['Edge16', 'Gauz12', 'Kell16', 'McMa13', 'Minn17', 'Lodi12.53', 'Lodi13.2474', 'Pena11', 'Pena12']
+    vista_ref_list = ['Edge16', 'Gauz12', 'Kell16', 'McMa13', 'Minn17', 'Lodi12.53', 'Lodi13.2474', 'Pena11', 'Pena12',
+                      'Smit18']
     gpi_ref_list = ['Garc17.162']
     visao_ref_list = ['Male14']
     ufti_name_list = ['ULAS J133502.11+150653.5']
