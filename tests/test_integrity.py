2d5740bd18d384708f81e8e6186fc8e5e3d42676
Ally Baldelli <67712811+Ajb2307@users.noreply.github.com>
2023-08-01 14:51:13 -0400

    test fixes

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -97,7 +97,7 @@ def test_publications(db):
             and_(db.Publications.c.doi.is_(''), db.Publications.c.bibcode.is_(None)),
             and_(db.Publications.c.doi.is_(None), db.Publications.c.bibcode.is_('')),
             and_(db.Publications.c.doi.is_(''), db.Publications.c.bibcode.is_('')))).astropy()
-    assert len(t) == 27, f'found {len(t)} publications with missing bibcode and doi'
+    assert len(t) == 28, f'found {len(t)} publications with missing bibcode and doi'
 
 
 def test_parameters(db):
