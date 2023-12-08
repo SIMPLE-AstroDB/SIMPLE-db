e6af0101ce03d6fed4309a6a990311229976db87
Lisha Ramon <lisharamontn@gmail.com>
2023-12-08 16:38:26 -0500

    push attempt

--- scripts/ingests/ingest_wise_1810-1010.py
+++ scripts/ingests/ingest_wise_1810-1010.py
@@ -5,7 +5,7 @@ from scripts.ingests.utils import *
 from astropy.table import Table
 import astropy.units as u
 from astropy.coordinates import Angle
- 
+  
 
 SAVE_DB = False  # True: save the data files(json) in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
