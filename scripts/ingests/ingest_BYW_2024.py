a912394a1673aec57c07d711691139f63fc5ed74
Lisha Ramon <lisharamontn@gmail.com>
2023-12-18 16:04:23 -0500

    Source ingest layout attempt

--- scripts/ingests/ingest_BYW_2024.py
+++ scripts/ingests/ingest_BYW_2024.py
@@ -6,7 +6,7 @@ import requests
 from io import StringIO
 from astropy.coordinates import Angle
 
-SAVE_DB = True  # save the data files in addition to modifying the .db file
+SAVE_DB = False  # save the data files in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
 # LOAD THE DATABASE
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
@@ -44,7 +44,9 @@ data_file = StringIO(data)
 
 #define the column names
 columns = ['source', 'ra', 'dec', 'epoch', 'equinox', 'shortname', 'reference', 'other_ref', 'comments']
+
 #read the csv data into an astropy table
+#ascii.read attempts to read data from local files rather from URLs so using a library like requests helps get data and create object that can be passed to ascii.read
 byw_table = ascii.read(link, format='csv', data_start=1, data_end=90, header_start=1, names=columns, guess=False, fast_reader=False, delimiter=',')
 
 #data_columns = ['Source', 'RA', 'Dec', 'Epoch', 'Equinox', 'Shortname', 'Reference', 'Other_ref', 'Comments']  # columns with wanted data values
@@ -54,14 +56,39 @@ byw_table = ascii.read(link, format='csv', data_start=1, data_end=90, header_sta
 print(byw_table)
 
 
+#Individual source ingest attempt...
 def ingest_source_one(db):
 
-    ingest_source_one(db, source = ["CWISE J000021.45-481314.9"], 
+    ingest_source(db, source = ["CWISE J000021.45-481314.9"], 
                   reference = "Rothermich", 
                   ra = [0.0893808], 
                   dec = [-48.2208077], 
                   epoch = [2015.4041], 
-                  equinox = "ICRS")
+                  equinox = "ICRS", 
+                  raise_error=True, 
+                  search_db=True, 
+                  other_reference=None, 
+                  comment=None)
+    
+
+
+#All sources ingest attempt...
+def ingest_all_sources(db):
+    ingest_sources(db, sources = [" "], 
+                   references=None, 
+                   ras=None, 
+                   decs=None, 
+                   comments=None, 
+                   epochs=None,
+                   equinoxes=None, 
+                   other_references=None, 
+                   raise_error=True, 
+                   search_db=True)   
+
+#calling functions    
+ingest_source_one(db)    
+OR
+#ingest_all_sources(db)    
     
 db.inventory('CWISE J000021.45-481314.9', pretty_print=True)
 
