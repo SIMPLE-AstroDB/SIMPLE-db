bc790e53bbb0b8fa09b9438023da87f07dea315b
Lisha Ramon <lisharamontn@gmail.com>
2023-12-27 15:03:40 -0500

    First spectra type ingest attempt

--- scripts/ingests/ingest_bard14_spectra.py
+++ scripts/ingests/ingest_bard14_spectra.py
@@ -10,7 +10,36 @@ RECREATE_DB = True  # recreates the .db file from the data files
 # LOAD THE DATABASE
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
 
-# SPECTRAL TYPES
+#Find how to read data from folder
+bard14 = ascii.read()
+
+# Read in file as Astropy table (Not sure how to ingest a folder, dont use csv)
+# file = 'bard14.csv'
+file = 'bard14.csv'
+data = Table.read('scripts/ingests/' + file)
+
+# print result astropy table
+print(bard14.info)
+
+#Ingest spectral types 
+#Loop through data 
+def ingest_all_spectral_types(db):
+    for row in file[1:90]:  
+
+        # Print spectral type information
+        print("Spectral Type Information:")
+        
+        for col_name in row.colnames:
+            print(f"{col_name}: {row[col_name]}")
+
+
+        print("-" * 20)
+
+#Call spectral types function
+ingest_all_spectral_types(db)
+
+
+# Ingest SPECTRAL TYPES, loop through data
 ingest_spectral_types(db, 
                           sources = " ", 
                           spectral_types= " ", 
@@ -18,3 +47,12 @@ ingest_spectral_types(db,
                           regimes = " ", 
                           spectral_type_error=None,
                           comments=None)
+
+#Idea to open a fits file
+#from astropy.io import fits
+#fits_image_filename = fits.util.get_testdata_filepath('test0.fits')
+#hdul = fits.open(fits_image_filename)
+
+# WRITE THE JSON FILES
+if SAVE_DB:
+    db.save_database(directory="data/")
