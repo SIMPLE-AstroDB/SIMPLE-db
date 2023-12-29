ee4f7a9a3afdd9de0c6de6711609dab1877a9d70
Lisha Ramon <lisharamontn@gmail.com>
2023-12-29 14:35:44 -0500

    Completed script, saved jsons, updated versions

--- scripts/ingests/ingest_bard14_spectra.py
+++ scripts/ingests/ingest_bard14_spectra.py
@@ -1,11 +1,10 @@
 from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
-from astropy.table import Table
 from astropy.io import ascii
-import astropy.units as u
+import requests
    
 
-SAVE_DB = False  # True: save the data files(json) in addition to modifying the .db file
+SAVE_DB = True  # True: save the data files(json) in addition to modifying the .db file
 RECREATE_DB = True  # recreates the .db file from the data files
 # LOAD THE DATABASE
 db = load_simpledb('SIMPLE.db', recreatedb=RECREATE_DB)
@@ -34,31 +33,50 @@ print(bard14_table.info)
           
 #function to update all spectra in the database
 def update_all_spectra(db):
-    with db.engine.begin() as conn:
-        for index, row in bard14_table.iterrows():
-            source_value = row['Source']
-            spectrum_value = row['Spectrum']
-            original_spectrum_value = row['Original Spectrum']          
+    
+    for row in bard14_table:
+        source_value = row['Source']
+        spectrum_value = row['Spectrum']
+        original_spectrum_value = row['Original Spectrum']          
 
 
-#update data in loop
-        conn.execute(db.Spectra.update()\
-                .where(db.Spectra.c.source == source_value)\
-                .values(spectrum = spectrum_value))
-        
+        print(source_value)
+        print(spectrum_value)
+        print(original_spectrum_value)
 
-        conn.execute(db.Spectra.update()\
-                .where(db.Spectra.c.source == source_value)\
-                .values(original_spectrum = original_spectrum_value))
+        # Add a separator between rows for better readability
+        print("-" * 50)
+
+
+        # The website is up if the status code is 200 (checking validity of links)
+        request_response = requests.head(spectrum_value)
+        status_code = (request_response.status_code)
+
+        if status_code != 200: 
+            msg = f"Link invalid:{spectrum_value}"
+            raise SimpleError(msg)  
+
+        if status_code != 200: 
+            msg = f"Link invalid:{original_spectrum_value}"
+            raise SimpleError(msg) 
+            
+
+        print(status_code) 
+
+
+        #update data in loop
+        with db.engine.begin() as conn:
+            conn.execute(db.Spectra.update()
+                    .where(db.Spectra.c.source == source_value)
+                    .values(spectrum = spectrum_value))
+    
+            conn.execute(db.Spectra.update()
+                    .where(db.Spectra.c.source == source_value)
+                    .values(original_spectrum = original_spectrum_value))
         
         
 #Call spectra function
 update_all_spectra(db)
-
-
-
-# Add a separator between rows for better readability
-#    print("-" * 50)
 
 
 # WRITE THE JSON FILES
