68b48ce3b3a1d873ee0691fc877c59b4976e7ac4
Lisha Ramon <lisharamontn@gmail.com>
2024-01-19 14:34:07 -0500

    missing sources ingest attempt

--- scripts/ingests/ingest_calamari_sample.py
+++ scripts/ingests/ingest_calamari_sample.py
@@ -11,7 +11,7 @@ db = load_simpledb("SIMPLE.db", recreatedb=RECREATE_DB)
 #Calamari url: https://docs.google.com/spreadsheets/d/1i_Iblqp4WoKJ8lbyCT_u_u7AOiyse3OHfzdQln-fi9g/edit#gid=0
 
 SHEET_ID = '1i_Iblqp4WoKJ8lbyCT_u_u7AOiyse3OHfzdQln-fi9g'
-SHEET_NAME = 'calamari'
+#SHEET_NAME = 'calamari'
 full = 'all'
 
 url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={full}'
@@ -29,6 +29,8 @@ calamari_table = ascii.read(
 #print result table
 print(calamari_table.info)
 
+found_list= [] 
+not_found_list= []
 
 
 for row in calamari_table:
@@ -40,9 +42,6 @@ for row in calamari_table:
     # check the target in the row is in the DB
     result = find_source_in_db(db, name, ra=RA, dec=Dec)
 
-    found_list= ['calamari_table'] #not sure what to assign these to
-    not_found_list= ['calamari_table']
-
     # add the target to its assigned list
     if len(result) > 0: # if it is in the database
         found_list.append(name)
@@ -52,6 +51,66 @@ for row in calamari_table:
 
 print(found_list)
 print(not_found_list)
+
+#Ingest missing sources (8 sources) also not found in SIMBAD
+#Need to ingest names, sources, publications, companion relationships
+sources = [
+    ["J19073307+3015304"],
+    ["J18005854+1505198"],
+    ["J11102921-2925186"],
+    ["ULAS J13300249+0914321"],
+    ["WISE J124332.17+600126.6"],
+    ["ULAS J014016.91+015054.7"],
+    ["HD 4747 B"],
+    ["HD 33632 Ab"]
+]
+#specific rows to ingest
+specific_rows = [6, 8, 10, 12, 32, 33, 39, 41]
+
+#idea is to loop through specific rows to ingest ra, dec and ref
+with calamari_table:
+    for row_index_calamari_table, row in enumerate(specific_rows):
+        if row_index_calamari_table in specific_rows:
+            #ingest data from specific row
+            print(row)
+
+#different idea
+def add_sources(db):
+
+    ingest_sources(db, ["J19073307+3015304"], references="", 
+            ras= ["286.8885"], 
+            decs=["30.25893"], 
+            search_db=False)
+    ingest_sources(db, ["J18005854+1505198"], references="", 
+            ras= [], 
+            decs=[], 
+            search_db=False)
+    ingest_sources(db, ["J11102921-2925186"], references="", 
+            ras= [], 
+            decs=[], 
+            search_db=False)
+    ingest_sources(db, ["ULAS J13300249+0914321"], references="", 
+            ras= [], 
+            decs=[], 
+            search_db=False)
+    ingest_sources(db, ["WISE J124332.17+600126.6"], references="", 
+            ras= [], 
+            decs=[], 
+            search_db=False)
+    ingest_sources(db, ["ULAS J014016.91+015054.7"], references="", 
+            ras= [], 
+            decs=[], 
+            search_db=False)
+    ingest_sources(db, ["HD 4747 B"], references="", 
+            ras= [], 
+            decs=[], 
+            search_db=False)
+    
+    ingest_sources(db, ["HD 33632 Ab"], references="", 
+            ras= [], 
+            decs=[], 
+            search_db=False)
+
 
 # WRITE THE JSON FILES
 if SAVE_DB:
