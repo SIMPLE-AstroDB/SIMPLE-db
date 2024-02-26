69b44015fdb7160d74efd156c8845dc838f3ab1f
kelle <kellecruz@gmail.com>
2024-02-26 16:26:03 -0600

    tidying

--- simple/schema.py
+++ simple/schema.py
@@ -2,12 +2,21 @@
 Schema for the SIMPLE database
 """
 
-# pylint: disable=line-too-long, missing-class-docstring, unused-import, invalid-name, singleton-comparison
+# pylint: disable=line-too-long, missing-class-docstring,
+# unused-import, invalid-name, singleton-comparison
 
 import enum
 import sqlalchemy as sa
-from sqlalchemy import Boolean, Column, Float, ForeignKey, Integer, String, \
-    BigInteger, Enum, Date, DateTime, ForeignKeyConstraint
+from sqlalchemy import (
+    Boolean,
+    Column,
+    Float,
+    ForeignKey,
+    String,
+    Enum,
+    DateTime,
+    ForeignKeyConstraint,
+)
 from astrodbkit2.astrodb import Base
 from astrodbkit2.views import view
 
@@ -16,7 +25,8 @@ from astrodbkit2.views import view
 # Reference tables
 class Publications(Base):
     """ORM for publications table.
-    This stores reference information (DOI, bibcodes, etc) and has shortname as the primary key
+    This stores reference information (DOI, bibcodes, etc)
+    and has shortname as the primary key
     """
     __tablename__ = 'Publications'
     reference = Column(String(30), primary_key=True, nullable=False)
@@ -29,16 +39,24 @@ class Telescopes(Base):
     __tablename__ = 'Telescopes'
     telescope = Column(String(30), primary_key=True, nullable=False)
     description = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'))
+    reference = Column(
+        String(30), ForeignKey("Publications.reference", onupdate="cascade")
+    )
 
 
 class Instruments(Base):
     __tablename__ = 'Instruments'
     instrument = Column(String(30), primary_key=True, nullable=False)
     mode = Column(String(30), primary_key=True)
-    telescope = Column(String(30), ForeignKey('Telescopes.telescope', onupdate='cascade'), primary_key=True)
+    telescope = Column(
+        String(30),
+        ForeignKey("Telescopes.telescope", onupdate="cascade"),
+        primary_key=True,
+    )
     description = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'))
+    reference = Column(
+        String(30), ForeignKey("Publications.reference", onupdate="cascade")
+    )
 
 
 class Parameters(Base):
@@ -51,10 +69,12 @@ class Parameters(Base):
 class PhotometryFilters(Base):
     """
     ORM for filter table.
-    This stores relationships between filters and instruments, telescopes, as well as wavelength and width
+    This stores relationships between filters and instruments, telescopes,
+    as well as wavelength and width
     """
     __tablename__ = 'PhotometryFilters'
-    band = Column(String(30), primary_key=True, nullable=False)  # of the form instrument.filter (see SVO)
+    band = Column(String(30), primary_key=True, nullable=False)
+    # of the form instrument.filter (see SVO)
     ucd = Column(String(100))
     effective_wavelength = Column(Float, nullable=False)
     width = Column(Float)
@@ -88,7 +108,8 @@ class Regimes(Base):
 
 class Regime(enum.Enum):
     """Enumeration for spectral type, spectra, and photometry regimes
-    Use UCD controlled vocabulary: https://www.ivoa.net/documents/UCD1+/20200212/PEN-UCDlist-1.4-20200212.html#tth_sEcB
+    Use UCD controlled vocabulary:
+    https://www.ivoa.net/documents/UCD1+/20200212/PEN-UCDlist-1.4-20200212.html#tth_sEcB
     The variable name is stored and used in the database; the string value should match it
     """
     ultraviolet = 'em.UV'
@@ -121,7 +142,8 @@ class Gravity(enum.Enum):
 # -------------------------------------------------------------------------------------------------------------------
 # Main tables
 class Sources(Base):
-    """ORM for the sources table. This stores the main identifiers for our objects along with ra and dec"""
+    """ORM for the sources table. This stores the main identifiers
+    for our objects along with ra and dec"""
     __tablename__ = 'Sources'
     source = Column(String(100), primary_key=True, nullable=False)
     ra = Column(Float)
@@ -129,68 +151,108 @@ class Sources(Base):
     epoch = Column(Float)  # decimal year
     equinox = Column(String(10))  # eg, J2000
     shortname = Column(String(30))  # not needed?
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), nullable=False)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        nullable=False,
+    )
     other_references = Column(String(100))
     comments = Column(String(1000))
 
 
 class Names(Base):
     __tablename__ = 'Names'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
     other_name = Column(String(100), primary_key=True, nullable=False)
 
 
 class Photometry(Base):
     # Table to store photometry information
     __tablename__ = 'Photometry'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
-    band = Column(String(30), ForeignKey('PhotometryFilters.band'), primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
+    band = Column(String(30), ForeignKey("PhotometryFilters.band"), primary_key=True)
     magnitude = Column(Float, nullable=False)
     magnitude_error = Column(Float)
     telescope = Column(String(30), ForeignKey('Telescopes.telescope'))
     epoch = Column(Float)  # decimal year
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), primary_key=True)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        primary_key=True,
+    )
 
 
 class Parallaxes(Base):
     # Table to store parallax values in milliarcseconds
     __tablename__ = 'Parallaxes'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
     parallax = Column(Float, nullable=False)
     parallax_error = Column(Float)
-    adopted = Column(Boolean)  # flag for indicating if this is the adopted measurement or not
+    adopted = Column(Boolean)  # flag for indicating if this is the adopted
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), primary_key=True)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        primary_key=True,
+    )
 
 
 class ProperMotions(Base):
     # Table to store proper motions, in milliarcseconds per year
     __tablename__ = 'ProperMotions'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
     mu_ra = Column(Float, nullable=False)
     mu_ra_error = Column(Float)
     mu_dec = Column(Float, nullable=False)
     mu_dec_error = Column(Float)
-    adopted = Column(Boolean)  # flag for indicating if this is the adopted measurement or not
+    adopted = Column(Boolean)
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), primary_key=True)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        primary_key=True,
+    )
 
 
 class RadialVelocities(Base):
     # Table to store radial velocities, in km/sec
     __tablename__ = 'RadialVelocities'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
     radial_velocity = Column(Float, nullable=False)
     radial_velocity_error = Column(Float)
-    adopted = Column(Boolean)  # flag for indicating if this is the adopted measurement or not
+    adopted = Column(Boolean)
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), primary_key=True)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        primary_key=True,
+    )
 
 
 class SpectralTypes(Base):
@@ -210,7 +272,7 @@ class SpectralTypes(Base):
     # regime = Column(
     #    String(30), ForeignKey("Regimes.regime", onupdate="cascade"), primary_key=True
     # )
-    adopted = Column(Boolean)  # flag for indicating if this is the adopted measurement or not
+    adopted = Column(Boolean)
     comments = Column(String(1000))
     reference = Column(
         String(30),
@@ -222,8 +284,12 @@ class SpectralTypes(Base):
 class Gravities(Base):
     # Table to store gravity measurements
     __tablename__ = 'Gravities'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
     gravity = Column(Enum(Gravity, create_constraint=True, native_enum=False),
                      nullable=False)  # restricts to enumerated values
     regime = Column(Enum(Regime, create_constraint=True, native_enum=False),
@@ -232,24 +298,42 @@ class Gravities(Base):
     #    String(30), ForeignKey("Regimes.regime", onupdate="cascade"), primary_key=True
     # )
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), primary_key=True)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        primary_key=True,
+    )
 
 
 class Spectra(Base):
     # Table to store references to spectra
     __tablename__ = 'Spectra'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
 
     # Data
     spectrum = Column(String(1000), nullable=False)  # URL of spectrum location
-    original_spectrum = Column(String(1000)) # URL of original spectrum location, if applicable
-    local_spectrum = Column(String(1000))  # local directory (via environment variable) of spectrum location
+    original_spectrum = Column(
+        String(1000)
+    )  # URL of original spectrum location, if applicable
+    local_spectrum = Column(
+        String(1000)
+    )  # local directory (via environment variable) of spectrum location
 
     # Metadata
-    regime = Column(Enum(Regime, create_constraint=True, values_callable=lambda x: [e.value for e in x],
-                         native_enum=False),
-                    primary_key=True)  # eg, Optical, Infrared, etc
+    regime = Column(
+        Enum(
+            Regime,
+            create_constraint=True,
+            values_callable=lambda x: [e.value for e in x],
+            native_enum=False,
+        ),
+        primary_key=True,
+    )  # eg, Optical, Infrared, etc
     # regime = Column(
     #    String(30), ForeignKey("Regimes.regime", onupdate="cascade"), primary_key=True
     # )
@@ -260,43 +344,70 @@ class Spectra(Base):
 
     # Common metadata
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), primary_key=True)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        primary_key=True,
+    )
     other_references = Column(String(100))
 
     # Composite Foreign key constraints for instrument and mode
-    __table_args__ = (ForeignKeyConstraint([telescope, instrument, mode],
-                                           [Instruments.telescope, Instruments.instrument, Instruments.mode],
-                                           onupdate="cascade"),
-                      {})
+    __table_args__ = (
+        ForeignKeyConstraint(
+            [telescope, instrument, mode],
+            [Instruments.telescope, Instruments.instrument, Instruments.mode],
+            onupdate="cascade",
+        ),
+        {},
+    )
 
 
 class ModeledParameters(Base):
     # Table to store derived/inferred paramaters from models
     __tablename__ = 'ModeledParameters'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
 
-    parameter = Column(String(30), ForeignKey('Parameters.parameter', onupdate='cascade'), primary_key=True)
+    parameter = Column(
+        String(30),
+        ForeignKey("Parameters.parameter", onupdate="cascade"),
+        primary_key=True,
+    )
     value = Column(Float, nullable=False)
     value_error = Column(Float)
     unit = Column(String(20))
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'), primary_key=True)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        primary_key=True,
+    )
 
 
 class CompanionRelationships(Base):
     # Table to store information about companions
     __tablename__ = 'CompanionRelationships'
-    source = Column(String(100), ForeignKey('Sources.source', ondelete='cascade', onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
     companion_name = Column(String(100), nullable=False, primary_key=True)
     projected_separation_arcsec = Column(Float)
     projected_separation_error = Column(Float)
-    relationship = Column(String(100), nullable=False) # Relationship of source to companion. 
+    relationship = Column(String(100), nullable=False)
+    # Relationship of source to companion.
     # Options: Child, Sibling, Parent, Unresolved Parent
     comments = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference', onupdate='cascade'))
-    other_companion_names = Column(String(10000)) # other names of the companions 
+    reference = Column(
+        String(30), ForeignKey("Publications.reference", onupdate="cascade")
+    )
+    other_companion_names = Column(String(10000))  # other names of the companions
 
 # -------------------------------------------------------------------------------------------------------------------
 # Views
@@ -306,32 +417,56 @@ ParallaxView = view(
     "ParallaxView",
     Base.metadata,
     sa.select(
-        Parallaxes.source.label('source'),
-        Parallaxes.parallax.label('parallax'),
-        Parallaxes.parallax_error.label('parallax_error'),
-        (1000./Parallaxes.parallax).label('distance'),  # distance in parsecs
-        Parallaxes.comments.label('comments'),
-        Parallaxes.reference.label('reference')
-    ).select_from(Parallaxes)
-    .where(sa.and_(Parallaxes.adopted == True, Parallaxes.parallax > 0)),
+        Parallaxes.source.label("source"),
+        Parallaxes.parallax.label("parallax"),
+        Parallaxes.parallax_error.label("parallax_error"),
+        (1000.0 / Parallaxes.parallax).label("distance"),  # distance in parsecs
+        Parallaxes.comments.label("comments"),
+        Parallaxes.reference.label("reference"),
+    )
+    .select_from(Parallaxes)
+    .where(sa.and_(Parallaxes.adopted is True, Parallaxes.parallax > 0)),
 )
 
 PhotometryView = view(
     "PhotometryView",
     Base.metadata,
     sa.select(
-        Photometry.source.label('source'),
-        sa.func.avg(sa.case((Photometry.band == "2MASS.J", Photometry.magnitude))).label("2MASS.J"),
-        sa.func.avg(sa.case((Photometry.band == "2MASS.H", Photometry.magnitude))).label("2MASS.H"),
-        sa.func.avg(sa.case((Photometry.band == "2MASS.Ks", Photometry.magnitude))).label("2MASS.Ks"),
-        sa.func.avg(sa.case((Photometry.band == "WISE.W1", Photometry.magnitude))).label("WISE.W1"),
-        sa.func.avg(sa.case((Photometry.band == "WISE.W2", Photometry.magnitude))).label("WISE.W2"),
-        sa.func.avg(sa.case((Photometry.band == "WISE.W3", Photometry.magnitude))).label("WISE.W3"),
-        sa.func.avg(sa.case((Photometry.band == "WISE.W4", Photometry.magnitude))).label("WISE.W4"),
-        sa.func.avg(sa.case((Photometry.band == "IRAC.I1", Photometry.magnitude))).label("IRAC.I1"),
-        sa.func.avg(sa.case((Photometry.band == "IRAC.I2", Photometry.magnitude))).label("IRAC.I2"),
-        sa.func.avg(sa.case((Photometry.band == "IRAC.I3", Photometry.magnitude))).label("IRAC.I3"),
-        sa.func.avg(sa.case((Photometry.band == "IRAC.I4", Photometry.magnitude))).label("IRAC.I4"),
-    ).select_from(Photometry)
-    .group_by(Photometry.source)
+        Photometry.source.label("source"),
+        sa.func.avg(
+            sa.case((Photometry.band == "2MASS.J", Photometry.magnitude))
+        ).label("2MASS.J"),
+        sa.func.avg(
+            sa.case((Photometry.band == "2MASS.H", Photometry.magnitude))
+        ).label("2MASS.H"),
+        sa.func.avg(
+            sa.case((Photometry.band == "2MASS.Ks", Photometry.magnitude))
+        ).label("2MASS.Ks"),
+        sa.func.avg(
+            sa.case((Photometry.band == "WISE.W1", Photometry.magnitude))
+        ).label("WISE.W1"),
+        sa.func.avg(
+            sa.case((Photometry.band == "WISE.W2", Photometry.magnitude))
+        ).label("WISE.W2"),
+        sa.func.avg(
+            sa.case((Photometry.band == "WISE.W3", Photometry.magnitude))
+        ).label("WISE.W3"),
+        sa.func.avg(
+            sa.case((Photometry.band == "WISE.W4", Photometry.magnitude))
+        ).label("WISE.W4"),
+        sa.func.avg(
+            sa.case((Photometry.band == "IRAC.I1", Photometry.magnitude))
+        ).label("IRAC.I1"),
+        sa.func.avg(
+            sa.case((Photometry.band == "IRAC.I2", Photometry.magnitude))
+        ).label("IRAC.I2"),
+        sa.func.avg(
+            sa.case((Photometry.band == "IRAC.I3", Photometry.magnitude))
+        ).label("IRAC.I3"),
+        sa.func.avg(
+            sa.case((Photometry.band == "IRAC.I4", Photometry.magnitude))
+        ).label("IRAC.I4"),
+    )
+    .select_from(Photometry)
+    .group_by(Photometry.source),
 )
