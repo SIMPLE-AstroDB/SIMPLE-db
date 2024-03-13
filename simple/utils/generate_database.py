7e5c36501f2fa5012caf35d4dec23077e8a6906c
kelle <kellecruz@gmail.com>
2024-03-13 15:50:40 -0400

    remove unused parts of script

--- simple/utils/generate_database.py
+++ simple/utils/generate_database.py
@@ -3,7 +3,6 @@
 
 import argparse
 import sys
-import os
 from astrodb_utils import load_astrodb
 
 sys.path.append("./")
@@ -17,27 +16,10 @@ DB_NAME = "SIMPLE.sqlite"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate the SIMPLE database")
-    # parser.add_argument(
-    #    "architecture",
-    #    choices=["sqlite", "postgres"],
-    #    help="Database architecture to use.",
-    # )
-    # parser.add_argument(
-    #    "connection_string",
-    #    nargs="?",
-    #    help="Connection string to use for non-sqlite databases.",
-    # )
-
     args = parser.parse_args()
 
-    # Get the connection string for any non-sqlite database
-    # if args.connection_string is not None:
-    #    connection_string = args.connection_string
-    # else:
-    connection_string = os.getenv("SIMPLE_DATABASE_URL", default="")
-
     # Run the loader for the specified DB architecture
-    db = load_astrodb(DB_NAME, reference_tables=REFERENCE_TABLES)
+    db = load_astrodb(DB_NAME, recreatedb=True, reference_tables=REFERENCE_TABLES)
     print("New database generated.")
 
     # Close all connections
