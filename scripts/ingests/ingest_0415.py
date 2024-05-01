fa49fe57d3355f84ce05d165967465f46e206ab3
Sherelyn Alejandro <80668767+SherelynA@users.noreply.github.com>
2024-05-01 02:22:39 -0400

    Adding sections for ingest

--- scripts/ingests/ingest_0415.py
+++ scripts/ingests/ingest_0415.py
@@ -1,3 +1,23 @@
-from scripts.ingests.ingest_utils import *
-from scripts.ingests.utils import *
+from astrodb_scripts import load_astrodb, ingest_instrument, ingest_publication
+from schema.schema import *
+from scripts.utils.ingest_spectra_utils import ingest_spectrum
+from scripts.utils.photometry import ingest_photometry_filter, ingest_photometry
+import logging
 
+SAVE_DB = False  # save the data files in addition to modifying the .db file
+RECREATE_DB = True  # recreates the .db file from the data files
+
+logger = logging.getLogger("AstroDB")
+logger.setLevel(logging.INFO)
+
+db = load_astrodb('SIMPLE.db', recreatedb=RECREATE_DB)
+
+# Spectra Files
+# Ingest Spectra
+# Ingest Photometry Filters
+# Ingest Photometry
+
+
+# WRITE THE JSON FILES
+if SAVE_DB:
+    db.save_database(directory='data/')
