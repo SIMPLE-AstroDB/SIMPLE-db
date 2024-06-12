f04ea398758da2b02968f95c34943fb67e72be4b
Evan <93949832+Exu-112@users.noreply.github.com>
2024-06-12 18:30:46 -0400

    actually updated the db.

--- scripts/ingests/ultracool_sheet/Ingest_radial_velocities.py
+++ scripts/ingests/ultracool_sheet/Ingest_radial_velocities.py
@@ -60,4 +60,6 @@ for source in uc_sheet_table:
             msg = f"Could not add {rv_data} to database."
             logger.warning(msg)
             #raise AstroDBError(msg) from e
+
+db.save_database(directory="data/")
         
