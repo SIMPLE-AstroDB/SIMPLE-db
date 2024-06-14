76d3335b1f502582993f15497750a848482eeefe
Sherelyn Alejandro <80668767+SherelynA@users.noreply.github.com>
2024-06-14 14:38:35 -0400

    Updating functions and files being called.

--- scripts/ingests/ingest_0415.py
+++ scripts/ingests/ingest_0415.py
@@ -1,7 +1,8 @@
-from astrodb_scripts import load_astrodb, ingest_instrument, ingest_publication
-from schema.schema import *
-from scripts.utils.ingest_spectra_utils import ingest_spectrum
-from scripts.utils.photometry import ingest_photometry_filter, ingest_photometry
+from astrodb_utils import load_astrodb, ingest_instrument, ingest_publication
+from simple.schema import *
+from simple.schema import REFERENCE_TABLES
+from simple.utils.spectra import ingest_spectrum
+from astrodb_utils.photometry import ingest_photometry_filter, ingest_photometry
 import logging
 
 SAVE_DB = False  # save the data files in addition to modifying the .db file
@@ -10,11 +11,14 @@ RECREATE_DB = True  # recreates the .db file from the data files
 logger = logging.getLogger("AstroDB")
 logger.setLevel(logging.INFO)
 
-db = load_astrodb('SIMPLE.db', recreatedb=RECREATE_DB)
+db = load_astrodb("SIMPLE.sqlite", recreatedb=True,  reference_tables=REFERENCE_TABLES)
 
 # Spectra Files
+
 # Ingest Spectra
+
 # Ingest Photometry Filters
+
 # Ingest Photometry
 
 
