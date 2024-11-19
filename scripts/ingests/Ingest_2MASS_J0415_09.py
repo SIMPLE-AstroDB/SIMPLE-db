0601763b91fcc1378ed412942871f05d74a8c411
canavarrete01 <100876091+canavarrete01@users.noreply.github.com>
2024-11-18 21:02:31 -0500

    Updating Ingest.py

--- scripts/ingests/Ingest_2MASS_J0415_09.py
+++ scripts/ingests/Ingest_2MASS_J0415_09.py
@@ -11,8 +11,8 @@ Key Functions:
 
 """
 
-from astrodb_utils import * # load_astrodb, ingest_instrument, ingest_publication, find_publication
-from simple.utils.spectra import * # ingest_spectrum
+from astrodb_utils import * 
+from simple.utils.spectra import * 
 from simple.schema import Spectra
 from simple.schema import *
 from simple.schema import REFERENCE_TABLES
@@ -20,95 +20,72 @@ from simple.schema import REFERENCE_TABLES
 #Load in Database
 db = load_astrodb("SIMPLE.sqlite", recreatedb=True, reference_tables=REFERENCE_TABLES)
 
-#Dictionary of new spectra
-spectras = [
-
-    {
-        "file": "https://bdnyc.s3.us-east-1.amazonaws.com/AKARI_2MASS%2BJ04151954-0935066_2007-08-23.fits",
-        "source": "2MASS J04151954-0935066",
-        "regime": "nir",
-        "telescope": "AKARI",
-        "instrument": "IRC",
-        "mode": "NIR",
-        "obs_date": "2007-08-23",
-        "bibcode": "2012ApJ...760..151S", 
-
-    },
-    {
-        "file": "https://bdnyc.s3.us-east-1.amazonaws.com/FIRE/FIRE_0415-0935.fits",
-        "source": "2MASS J04151954-0935066",
-        "regime": "nir",
-        "telescope": "Magellan I Baade",
-        "instrument": "FIRE",
-        "mode": "Echelle",
-        "obs_date": "2010-09-20",
-        "reference": "Simc13",
-        "bibcode": "2013PASP..125..270S", 
-    },
-    {
-        "file": "https://bdnyc.s3.us-east-1.amazonaws.com/LRIS/KECK_LRIS_2MASSJ04151954-0935066_T8_LRIS_Burg03.fits",
-        "source": "2MASS J04151954-0935066",
-        "regime": "nir",
-        "telescope": "KECK I",
-        "instrument": "LRIS",
-        "mode": "Missing",
-        "obs_date": "2001-02-20",
-        "reference": "Burg03",
-        "comments": "The Spectra of T Dwarfs. II. Red Optical Data",
-        "bibcode": "2003ApJ...594..510B", 
-        "doi": "10.1086/376756",
-
-    },
-    {
-        "file": "https://bdnyc.s3.us-east-1.amazonaws.com/LRIS/KECK_LRIS_2MASS_J04151954-0935066_2006-12-26.fits",
-        "source": "2MASS J04151954-0935066",
-        "regime": "nir",
-        "telescope": "KECK I",
-        "instrument": "LRIS",
-        "mode": "Missing",
-        "obs_date": "2006-12-26",
-        "reference": "Kirk08",
-        "comments": "A Sample of Very Young Field L Dwarfs and Implications for the Brown Dwarf 'Lithium Test' at Early Ages",
-        "bibcode": "2008ApJ...689.1295K", 
-        "doi": "10.1086/592768",
-    }
-]
-
-
 #Ingest data
-for spectrum in spectras:
-    
-    ingest_instrument(
-        db,
-        telescope=spectrum["telescope"],
-        instrument=spectrum["instrument"],
-        mode=spectrum["mode"],
-    )
+ingest_publication(
+    db, 
+    bibcode = "2012ApJ...760..151S"
+)
 
-    status, details = find_publication(db, reference=spectrum["reference"])
-    if status == False:
-        ingest_publication(
-            db,
-            reference=spectrum["reference"],
-            bibcode=spectrum['bibcode'],
-            doi=spectrum['doi'],
-            ignore_ads=True,
-        )
+ingest_publication(
+    db, 
+    bibcode = "2013PASP..125..270S"
+)
 
+ingest_publication(
+    db, 
+    bibcode="2007PASJ...59S.369M"
+)
 
-    ingest_spectrum(
-        db,
-        source=spectrum["source"],
-        spectrum=spectrum["file"],
-        regime=spectrum["regime"],
-        telescope=spectrum["telescope"],
-        instrument=spectrum["instrument"],
-        mode=spectrum["mode"],
-        obs_date=spectrum["obs_date"],
-        reference=spectrum["reference"],
-        comments=spectrum['comments']
+ingest_spectrum(
+    db,
+    source="2MASS J04151954-0935066",
+    spectrum="https://bdnyc.s3.us-east-1.amazonaws.com/AKARI_2MASS%2BJ04151954-0935066_2007-08-23.fits",
+    regime="nir",
+    telescope="AKARI",
+    instrument="IRC",
+    mode="NIR",
+    obs_date="2007-08-23",
+    reference="Sora12"
 
-    )
+)
+
+ingest_spectrum(
+    db,
+    source="2MASS J04151954-0935066",
+    spectrum="https://bdnyc.s3.us-east-1.amazonaws.com/FIRE/FIRE_0415-0935.fits",
+    regime="nir",
+    telescope="Magellan I Baade",
+    instrument="FIRE",
+    mode="Echelle",
+    obs_date="2010-09-20",
+    reference="Simc13"
+
+)
+
+ingest_spectrum(
+    db,
+    source="2MASS J04151954-0935066",
+    spectrum="https://bdnyc.s3.us-east-1.amazonaws.com/LRIS/KECK_LRIS_2MASS_J04151954-0935066_2006-12-26.fits",
+    regime="nir",
+    instrument="LRIS",
+    telescope="Keck I",
+    mode="Missing",
+    obs_date="2006-12-26",
+    reference="Kirk08"
+)
+
+ingest_spectrum(
+    db,
+    source="2MASS J04151954-0935066",
+    spectrum="https://bdnyc.s3.us-east-1.amazonaws.com/LRIS/KECK_LRIS_2MASSJ04151954-0935066_T8_LRIS_Burg03.fits",
+    regime="nir",
+    instrument="LRIS",
+    telescope="Keck I",
+    mode="Missing",
+    obs_date="2001-02-20",
+    reference="Burg03.510"
+)
+
 
 # Save to Database
 db.save_database(directory="data/")
