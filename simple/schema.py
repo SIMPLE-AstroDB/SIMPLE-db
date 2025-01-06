c08a4c52482589cd3f2f156be84d8df496f7ddc1
canavarrete01 <100876091+canavarrete01@users.noreply.github.com>
2025-01-06 15:23:41 -0500

    Schema Update + Script Addition

--- simple/schema.py
+++ simple/schema.py
@@ -432,12 +432,14 @@ class ModeledParameters(Base):
     value = Column(Float, nullable=False)
     value_error = Column(Float)
     unit = Column(String(20))
+    model = Column(String(20))
     comments = Column(String(1000))
     reference = Column(
         String(30),
         ForeignKey("Publications.reference", onupdate="cascade"),
         primary_key=True,
     )
+    
 
 
 class CompanionRelationships(Base):
