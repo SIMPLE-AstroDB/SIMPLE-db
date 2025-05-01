9d2732afff6a456fa3b203338173859a137a3b75
canavarrete01 <100876091+canavarrete01@users.noreply.github.com>
2025-05-01 16:31:45 -0400

    Ingesting Spectra for B24

---
+++ scripts/ingests/beiler24/ingestsources_b24.py
@@ -0,0 +1,84 @@
+## Ingest Sources for Beiler24 ----
+# SIMPLE & Astrodb Packages
+from astrodb_utils import load_astrodb
+from astrodb_utils.sources import ingest_source, find_source_in_db, ingest_name
+from astrodb_utils.publications import ingest_publication
+from simple import REFERENCE_TABLES
+import os
+import openpyxl
+import pandas as pd
+
+# Load Database
+recreate_db = False
+save_db = True
+
+SCHEMA_PATH = "simple/schema.yaml"   
+db = load_astrodb(
+    "SIMPLE.sqlite",
+    recreatedb=recreate_db,  
+    reference_tables=REFERENCE_TABLES, 
+    felis_schema=SCHEMA_PATH)
+
+# Read the Excel file
+path = "/Users/carolina/Documents/AMNH/SIMPLE/SIMPLE-db/scripts/ingests/beiler24/"
+excel_path = os.path.join(path, "Beiler_SIMPLE_Ingest.xlsx")
+data = pd.read_excel(excel_path)
+sources_added = 0
+spectra_added = 0
+
+# Ingest Sources ----
+for _, row in data.iterrows():
+    try:
+        ingest_source(
+            db,
+            source=row['source'],
+            reference="Beil24",  
+        )
+        print(f"Source {row['source']} ingested.")
+        sources_added =+ 1
+
+    except Exception as e:
+        print(f"Error ingesting source {row['source']}: {e}")
+        continue
+
+print(f"Total sources added: {sources_added}")
+
+# Ingest Spectra ----
+""" Note: Any source containing '+' has been replaced with '%2B' in the AWS URL"""
+
+url = "https://bdnyc.s3.us-east-1.amazonaws.com/Beiler24/"
+
+for _, row in data.iterrows():
+    try:
+        source_name = row['source']
+        spectrum_file = row['spectrum permalink']
+        if '+' in spectrum_file:
+            spectrum_file = spectrum_file.replace("+", "%2B")
+        spectrum_url = url + spectrum_file
+        
+        ingest_spectrum = {
+            "db": db,
+            "source": source_name,
+            "spectrum": spectrum_url,
+            "regime": row['regime'],
+            "telescope": row['telescope'],
+            "instrument": row['instrument'],
+            "mode": row['mode'],
+            "obs_date": row['observation_date'],
+            "reference": "Beil24",
+            "bibcode": "2024ApJ...973..107B"
+        }
+        
+        
+        print(f"Spectrum for {source_name} ingested.")
+        spectra_added += 1
+
+    except Exception as e:
+        print(f"Error ingesting spectrum for {source_name}: {e}")
+        continue
+
+print(f"Total spectra added: {spectra_added}")
+
+# Save to Database, Writes the JSON Files
+if save_db: 
+    db.save_database(directory="data/")
