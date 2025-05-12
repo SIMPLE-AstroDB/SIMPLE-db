0be1be794652426efb91a5ba8035156b561c7ae2
kelle <kellecruz@gmail.com>
2025-05-12 19:00:38 -0400

    logger improvemetns

--- simple/__init__.py
+++ simple/__init__.py
@@ -1,9 +1,3 @@
-import logging
-import sys
-#from .version import version as __version__
-
-__all__ = ["__version__"]
-
 REFERENCE_TABLES = [
     "Publications",
     "Telescopes",
@@ -14,15 +8,3 @@ REFERENCE_TABLES = [
     "Parameters",
     "Regimes",
 ]
-
-logger = logging.getLogger(__name__)
-
-LOGFORMAT = logging.Formatter("%(levelname)-8s - %(name)-15s - %(message)s")
-
-handler = logging.StreamHandler(stream=sys.stdout)
-handler.setFormatter(LOGFORMAT)
-logger.addHandler(handler)
-
-logger.setLevel(logging.INFO)
-logger.info("SIMPLE logger initialized")
-logger.info(f"Logger level: {logging.getLevelName(logger.getEffectiveLevel()) }")
