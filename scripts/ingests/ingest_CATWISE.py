3ebba408a2fa565195ba8de4fc3b7b1be26bb262
kaseyLee123 <142427883+kaseyLee123@users.noreply.github.com>
2025-06-18 17:36:48 -0400

    need to find references for proper motions somehow...

--- scripts/ingests/ingest_CATWISE.py
+++ scripts/ingests/ingest_CATWISE.py
@@ -5,7 +5,8 @@ from simple import *
 from simple import REFERENCE_TABLES
 from astropy.io import ascii
 from astrodb_utils.publications import (
-    logger
+    logger,
+    find_publication
 )
 from sqlalchemy import select
 from astroquery.ipac.irsa import Irsa #we will use astroquery 
@@ -35,34 +36,30 @@ for row in sources:
 
     # generates a list of objects from the catwise2020 catalogs that are within this radius of a certain position/coordinate
     results = Irsa.query_region(coordinates=coord, spatial='Cone', catalog='catwise_2020', radius=2 * u.arcmin, columns="source_name,PMRA,sigPMRA,PMDec,sigPMDec,ab_flags,cc_flags,w1mpro,w1sigmpro,w2mpro,w2sigmpro")
-    print(results)
+
     # generates a list of filtered results
     filtered_results = []
     for source in results:
        if (source["ab_flags"] == '00') & (source["cc_flags"] == '0000'):
         filtered_results.append(source["source_name"])
     
-    print(filtered_results)
     #use closest/best match
     match = filtered_results[0]
     logger.info("source match found")
 
-
     source_row = results[results["source_name"] == match]
-    print(source_row)
-    print("\n")
-    print(source_row[0])
 
     with db.engine.connect() as conn:
         #ingest proper motions
         conn.execute(
                 db.ProperMotions.insert().values(
                     {
-                        "source": source_row["source_name"],
-                        "mu_ra": source_row["pmra"],
-                        "mu_ra_error": source_row["sigpmra"],
-                        "mu_dec": source_row["pmdec"],
-                        "mu_dec_error": source_row["sigpmdec"]
+                        "source": source_row[0][0],
+                        "mu_ra": source_row[0][1],
+                        "mu_ra_error": source_row[0][2],
+                        "mu_dec": source_row[0][3],
+                        "mu_dec_error": source_row[0][4],
+                        "reference": find_publication(source_row[0][0])
                     }
                 )
         )
@@ -70,10 +67,10 @@ for row in sources:
         conn.execute(
                 db.Photometry.insert().values(
                     {
-                        "source": source_row["source_name"],
+                        "source": source_row[0][0],
                         "band": 1,
-                        "magnitude": source_row["w1mpro"],
-                        "magnitude_error": source_row["w1sigmpro"],
+                        "magnitude": source_row[0][7],
+                        "magnitude_error": source_row[0][8],
                     }
                 )
         )
@@ -83,8 +80,8 @@ for row in sources:
                     {
                         "source": source_row["source_name"],
                         "band": 2,
-                        "magnitude": source_row["w2mpro"],
-                        "magnitude_error": source_row["w2sigmpro"],
+                        "magnitude": source_row[0][9],
+                        "magnitude_error": source_row[0][10],
                     }
                 )
         )
