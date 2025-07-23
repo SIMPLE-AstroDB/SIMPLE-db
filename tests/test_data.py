be0c3f2bce679c0d0dbb41702e78c95f9a909e83
kaseyLee123 <142427883+kaseyLee123@users.noreply.github.com>
2025-07-23 17:56:46 -0400

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -21,7 +21,7 @@ def test_sources(db):
     assert n_sources == 3618, f"found {n_sources} sources"
 
     n_names = db.query(db.Names).count()
-    assert n_names == 12145, f"found {n_names} names"
+    assert n_names == 12187, f"found {n_names} names"
 
 
 @pytest.mark.parametrize(
@@ -120,7 +120,7 @@ def test_missions(db):
     )
     s = db.session.scalars(stm).all()
     assert (
-        len(s) == 159
+        len(s) == 160
     ), f"found {len(s)} sources with WISE designation that have no WISE photometry"
     
 
