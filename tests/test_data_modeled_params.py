cd56a9e6475a64590c21cb468be658dbba205e91
kaseyLee123 <142427883+kaseyLee123@users.noreply.github.com>
2025-07-30 18:54:15 -0400

    updated csv files + versions + pytest

--- tests/test_data_modeled_params.py
+++ tests/test_data_modeled_params.py
@@ -8,9 +8,6 @@ def test_model(db):
         .filter(db.ModeledParameters.c.model.is_(None))
         .astropy()
     )
-    if len(t) > 0:
-        print("\nEntries found without a model")
-        print(t)
     assert len(t) == 705
 
 
@@ -35,3 +32,46 @@ def test_modeledparameters_params(db, param, n_counts):
     assert (
         len(t) == n_counts
     ), f"found {len(t)} modeled parameters with {param} parameter"
+
+@pytest.mark.parametrize(
+    ("ref", "param", "n_counts"),
+    [
+        ("Sanghi23", "T eff", 0),
+        ("Sanghi23", "log g", 0),
+        ("Sanghi23", "mass", 0),
+        ("Sanghi23", "radius", 0),
+        ("Sanghi23", "metallicity", 0),
+        ("Sanghi23", "L bol", 0),
+        ("Fili15", "T eff", 0),
+        ("Fili15", "log g", 0),
+        ("Fili15", "mass", 0),
+        ("Fili15", "radius", 0),
+        ("Fili15", "metallicity", 0),
+        ("Fili15", "L bol", 0),
+        ("Zhan19.1423", "T eff", 0),
+        ("Zhan19.1423", "log g", 0),
+        ("Zhan19.1423", "mass", 0),
+        ("Zhan19.1423", "radius", 0),
+        ("Zhan19.1423", "metallicity", 0),
+        ("Zhan19.1423", "L bol", 0),
+        ("Lodi22", "T eff", 0),
+        ("Lodi22", "log g", 0),
+        ("Lodi22", "mass", 0),
+        ("Lodi22", "radius", 0),
+        ("Lodi22", "metallicity", 0),
+        ("Lodi22", "L bol", 0),
+    ],
+)
+def test_modeledparameters_param_refs(db, ref, param, n_counts):
+    t = (
+        db.query(db.ModeledParameters)
+        .filter(
+            db.ModeledParameters.c.parameter == param,
+            db.ModeledParameters.c.reference == ref
+        )
+        .astropy()
+    )
+
+    assert (
+        len(t) == n_counts
+    ), f"found {len(t)} {param} parameters derived from {ref} reference"
