aedd2288eaf3ad27c9ad0ac13fac7f8d7b818f54
kelle <kellecruz@gmail.com>
2026-04-14 16:45:39 -0400

    update generate_database to use new functions

--- simple/utils/generate_database.py
+++ simple/utils/generate_database.py
@@ -2,30 +2,14 @@
 # This gets run automatically with Github Actions
 
 import argparse
-import sys
-from astrodb_utils import load_astrodb
-
-sys.path.append("./")
-from simple import REFERENCE_TABLES
-
-# Location of source data
-DB_NAME = "SIMPLE.sqlite"
-DB_PATH = "data/"
-SCHEMA_PATH = "simple/schema.yaml"
-
+from astrodb_utils.loaders import build_db_from_json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate the SIMPLE database")
     args = parser.parse_args()
 
-    # Run the loader for the specified DB architecture
-    db = load_astrodb(
-        DB_NAME,
-        data_path=DB_PATH,
-        recreatedb=True,
-        reference_tables=REFERENCE_TABLES,
-        felis_schema=SCHEMA_PATH,
-    )
+    db = build_db_from_json(settings_file="database.toml")
+
     print("New database generated.")
 
     # Close all connections
